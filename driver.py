9c7d2257c42a242bb507143e4d2b517b4a105430
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-05-11 16:45:39 +0800

    update CPR setup

--- driver.py
+++ driver.py
@@ -81,12 +81,13 @@ def setup_experiment(script_path, script_name):
     execute_command(script_command)
 
 
-def cpr(deploy_dir_path, setup_dir_path, bug_id):
+def cpr(setup_dir_path, bug_id):
     global CONF_TOOL_PARAMS, CONF_TOOL_PATH, CONF_TOOL_NAME, DIR_LOGS
     print("\t[INFO] running CPR")
     conf_path = setup_dir_path + "/cpr/repair.conf"
-    script_path = "setup.sh"
+    script_path = "cpr/setup.sh"
     log_path = str(conf_path).replace(".conf", ".log")
+    setup_dir_path = setup_dir_path + "/cpr"
     setup_command = "cd " + setup_dir_path + "; bash " + script_path + " " + CONF_DATA_PATH
     execute_command(setup_command)
     tool_command = "{ " + CONF_TOOL_NAME + " --conf=" + conf_path + " " + CONF_TOOL_PARAMS + ";} 2> " + FILE_ERROR_LOG
