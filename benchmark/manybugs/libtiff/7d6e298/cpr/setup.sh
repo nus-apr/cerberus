2d7d2141003c4624a080816cccb3e6c036931c41
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-05-25 01:54:58 +0800

    update scripts for cpr

--- benchmark/manybugs/libtiff/7d6e298/cpr/setup.sh
+++ benchmark/manybugs/libtiff/7d6e298/cpr/setup.sh
@@ -1,54 +1,8 @@
 project_name=libtiff
 bug_id=7d6e298
 dir_name=$1/manybugs/$project_name/$bug_id
-download_url=https://repairbenchmarks.cs.umass.edu/ManyBugs/scenarios/libtiff-bug-2007-07-19-ce4b7af-7d6e298.tar
 current_dir=$PWD
-mkdir -p $dir_name
-cd $dir_name
-#wget $download_url
-cp $current_dir/libtiff-bug-2007-07-19-ce4b7af-7d6e298.tar .
-tar xf libtiff-bug-2007-07-19-ce4b7af-7d6e298.tar
-mv libtiff-bug-2007-07-19-ce4b7af-7d6e298 src
-rm libtiff-bug-2007-07-19-ce4b7af-7d6e298.tar
-mv src/* .
-rm -rf src
-rm -rf  coverage* \
-        configuration-oracle \
-        local-root \
-        limit* \
-        *.cache \
-        *.debug.* \
-        sanity \
-        compile.pl \
-        *~ \
-        test \
-        reconfigure \
-        preprocessed \
-        fixed-program.txt
-mv bugged-program.txt manifest.txt
-mv *.lines bug-info
-mv fix-failures bug-info
-mv libtiff src
 cd $dir_name/src
-cp $current_dir/tif_dirinfo.c ./libtiff/tif_dirinfo.c
-make distclean
-chown -R root $dir_name
-
-## Compile libtiff.
-make clean
-CC=wllvm CXX=wllvm++ ./configure CFLAGS='-g -O0' --enable-static --disable-shared
-sed -i '978 s/./\t&/' test/Makefile
-CC=wllvm CXX=wllvm++ make CFLAGS="-march=x86-64" -j32
-
-cd $dir_name
-
-## fix the test harness and the configuration script
-sed -i "s#/root/mountpoint-genprog/genprog-many-bugs/libtiff-bug-2007-07-19-ce4b7af-7d6e298#/data/manybugs/libtiff/7d6e298#g" test.sh
-sed -i "s#/data/manybugs/libtiff/7d6e298/src/limit#timeout 5#g" test.sh
-sed -i "s#/usr/bin/perl#perl#g" test.sh
-sed -i "s#cd libtiff#cd src#g" test.sh
-
-cd src
 
 ## Prepare for KLEE
 # Fix fabs calls (not supported by KLEE).
@@ -92,18 +46,3 @@ cp test-input $dir_name
 cp -rf components $dir_name
 cp -rf test-input-files $dir_name
 cp -rf test-expected-output $dir_name
-#cp -rf seed-dir $dir_name
-
-#### Test with KLEE
-#cd /data/manybugs/libtiff/865f7b2/src/tools
-#klee --posix-runtime --libc=uclibc --link-llvm-lib=/concolic-repair/lib/libtrident_runtime.bca --write-smt2s tiffcp.bc /data/manybugs/libtiff/865f7b2/test-input-files/22-44-54-64-74-fail-palette-1c-1b.tiff test.tif
-#klee --posix-runtime --libc=uclibc --link-llvm-lib=/concolic-repair/lib/libtrident_runtime.bca --write-smt2s tiffcp.bc /data/manybugs/libtiff/865f7b2/test-input-files/13-14-15-16-17-22-43-53-63-73-fail-miniswhite-1c-1b.tiff test.tif
-#klee --posix-runtime --libc=uclibc --link-llvm-lib=/concolic-repair/lib/libtrident_runtime.bca --write-smt2s tiffcp.bc /data/manybugs/libtiff/865f7b2/seed-dir/2-pass-long_test.tiff test.tif
-#klee --posix-runtime --libc=uclibc --link-llvm-lib=/concolic-repair/lib/libtrident_runtime.bca --write-smt2s tiffcp.bc /data/manybugs/libtiff/865f7b2/seed-dir/22-40-50-60-70-pass-minisblack-1c-16b.tiff test.tif
-##
-#cd /data/manybugs/libtiff/865f7b2/test-input-files
-#gen-bout --sym-file "/data/manybugs/libtiff/865f7b2/test-input-files/22-44-54-64-74-fail-palette-1c-1b.tiff"
-#cd /data/manybugs/libtiff/865f7b2/src/tools
-#klee --posix-runtime --libc=uclibc --link-llvm-lib=/concolic-repair/lib/libtrident_runtime.bca --write-smt2s --seed-out=/data/manybugs/libtiff/865f7b2/test-input-files/file.bout --allow-seed-extension --resolve-path --named-seed-matching tiffcp.bc A --sym-files 1 3312 test.tif
-#klee --posix-runtime --libc=uclibc --link-llvm-lib=/CPR/lib/libtrident_runtime.bca --write-smt2s --seed-out=/data/manybugs/libtiff/865f7b2/test-input-files/file.bout --allow-seed-extension --resolve-path --named-seed-matching tiffcp.bc A --sym-files 1 3312 test.tif
-##
