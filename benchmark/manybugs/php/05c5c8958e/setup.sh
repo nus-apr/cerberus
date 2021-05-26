cefd55f9928cff1cfe5b150ecee934e34aa7d553
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-05-26 10:54:03 +0800

    add setup scripts for php

---
+++ benchmark/manybugs/php/05c5c8958e/setup.sh
@@ -0,0 +1,58 @@
+project_name=php
+bug_id=05c5c8958e
+scenario_id=php-bug-2011-10-15-0a1cc5f01c-05c5c8958e
+diff_file=Zend/zend_compile.c-0a1cc5f01c
+dir_name=$1/manybugs/$project_name/$bug_id
+download_url=https://repairbenchmarks.cs.umass.edu/ManyBugs/scenarios/${scenario_id}.tar.gz
+current_dir=$PWD
+mkdir -p $dir_name
+cd $dir_name
+wget $download_url
+tar xf ${scenario_id}.tar.gz
+mv ${scenario_id} src
+rm ${scenario_id}.tar.gz
+mv src/* .
+rm -rf src
+rm -rf  coverage* \
+        configuration-oracle \
+        local-root \
+        limit* \
+        *.cache \
+        *.debug.* \
+        sanity \
+        compile.pl \
+        *~ \
+        test \
+        reconfigure \
+        preprocessed \
+        fixed-program.txt
+mv bugged-program.txt manifest.txt
+mv *.lines bug-info
+mv fix-failures bug-info
+mv $project_name src
+cd $dir_name/src
+cp $dir_name/diffs/${diff_file} $dir_name/src/$(echo $diff_file| cut -d'-' -f 1)
+make distclean
+chown -R root $dir_name
+
+
+# COMPILE PHP
+cd src/php
+make clean
+CC=wllvm CXX=wllvm++  ./configure \
+  --enable-cli \
+  --disable-dom \
+  --disable-libxml  \
+  --disable-xml \
+  --disable-simplexml \
+  --disable-xmlreader  \
+  --disable-xmlwriter  \
+  --disable-pear  \
+  --disable-phar \
+  --disable-inline-optimization  \
+  --without-pcre-dir  \
+  --disable-fileinfo \
+  --disable-shared
+
+CC=clang CXX=clang++ make  -j32
+
