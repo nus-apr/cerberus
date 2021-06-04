b6359afe44aa8ae88e4b2ac2cf244a343297db5b
rshariffdeen <rshariffdeen@gmail.com>
2021-06-04 10:55:24 +0800

    split setup script to setup/config/build/test

--- benchmark/manybugs/libtiff/0a36d7f/setup.sh
+++ benchmark/manybugs/libtiff/0a36d7f/setup.sh
@@ -1,8 +1,12 @@
-project_name=libtiff
-bug_id=0a36d7f
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=/data/$benchmark_name/$project_name/$bug_id
+
 scenario_id=libtiff-bug-2006-03-03-a72cf60-0a36d7f
 diff_file=libtiff/tif_dirread.c-a72cf60
-dir_name=$1/manybugs/$project_name/$bug_id
+
 download_url=https://repairbenchmarks.cs.umass.edu/ManyBugs/scenarios/${scenario_id}.tar.gz
 current_dir=$PWD
 mkdir -p $dir_name
@@ -32,26 +36,5 @@ mv fix-failures bug-info
 mv $project_name src
 cd $dir_name/src
 cp $dir_name/diffs/${diff_file} $dir_name/src/$(echo $diff_file| cut -d'-' -f 1)
-make distclean
 chown -R root $dir_name
-
-# Compile libtiff.
-make clean
-CC=wllvm CXX=wllvm++ ./configure CFLAGS='-g -O0' --enable-static --disable-shared
-sed -i '978 s/./\t&/' test/Makefile
-CC=wllvm CXX=wllvm++ make CFLAGS="-march=x86-64" -j32
-
-cd $dir_name
-
-## fix the test harness and the configuration script
-sed -i "s#/root/mountpoint-genprog/genprog-many-bugs/${scenario_id}#/data/manybugs/libtiff/${bug_id}#g" test.sh
-sed -i "s#/data/manybugs/libtiff/${bug_id}/limit#timeout 5#g" test.sh
-sed -i "s#/usr/bin/perl#perl#g" test.sh
-sed -i "s#cd libtiff#cd src#g" test.sh
-
-# Run few test cases
-bash test.sh p1 /data
-bash test.sh p3 /data
-bash test.sh p5 /data
-bash test.sh p7 /data
-bash test.sh p9 /data
+echo -ne 'all:\nclean:\ndistclean:\n' >> contrib/Makefile
