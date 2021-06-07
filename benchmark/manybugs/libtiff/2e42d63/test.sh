e253071e807b89e21d7deb8bd90a83889f2c9d63
rshariffdeen <rshariffdeen@gmail.com>
2021-06-07 16:55:01 +0800

    update test scripts for libtiff

--- benchmark/manybugs/libtiff/2e42d63/test.sh
+++ benchmark/manybugs/libtiff/2e42d63/test.sh
@@ -3,11 +3,24 @@ benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=/data/$benchmark_name/$project_name/$bug_id
-cd $dir_name/src
+scenario_id=libtiff-bug-2009-02-05-764dbba-2e42d63
+cd $dir_name
 
-# Run few test cases
-bash test.sh p1 /data
-bash test.sh p3 /data
-bash test.sh p5 /data
-bash test.sh p7 /data
-bash test.sh p9 /data
+
+## fix the test harness and the configuration script
+sed -i "s#/root/mountpoint-genprog/genprog-many-bugs/${scenario_id}#/data/manybugs/libtiff/${bug_id}#g" test.sh
+sed -i "s#/data/manybugs/libtiff/${bug_id}/limit#timeout 5#g" test.sh
+sed -i "s#/usr/bin/perl#perl#g" test.sh
+sed -i "s#cd libtiff#cd src#g" test.sh
+
+# Run passing test cases
+for i in `seq -s " " -f "p%g"  1 64`
+do
+bash test.sh $i /data
+done
+
+# Run failing test cases
+for i in `seq -s " " -f "n%g"  1 9`
+do
+bash test.sh $i /data
+done
