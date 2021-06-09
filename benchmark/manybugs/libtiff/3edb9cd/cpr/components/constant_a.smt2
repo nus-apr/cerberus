11c38586dbb89ae35f4ccfcc9397babf45d4f7b4
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-06-10 02:20:27 +0800

    update libtiff subject for cpr

---
+++ benchmark/manybugs/libtiff/3edb9cd/cpr/components/constant_a.smt2
@@ -0,0 +1,3 @@
+(declare-const const_a (_ BitVec 32))
+(declare-const rreturn (_ BitVec 32))
+(assert (= rreturn const_a))
---
+++ benchmark/manybugs/libtiff/3edb9cd/cpr/instrument.sh
@@ -0,0 +1,53 @@
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 4 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+fix_id=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+dir_name=/data/$benchmark_name/$project_name/$fix_id
+
+cd $dir_name/src
+make clean
+
+if [ ! -f "$dir_name/src/INSTRUMENTED_CPR" ]; then
+    touch "$dir_name/src/INSTRUMENTED_CPR"
+fi
+
+
+## Prepare for KLEE
+# Fix fabs calls (not supported by KLEE).
+sed -i 's/fabs/fabs_trident/g' libtiff/tif_luv.c
+sed -i 's/fabs/fabs_trident/g' tools/tiff2ps.c
+#sed -i 's/fabs_trident/fabs/g' libtiff/tif_luv.c
+#sed -i 's/fabs_trident/fabs/g' tools/tiff2ps.c
+
+make CC=$TRIDENT_CC CXX=$TRIDENT_CXX -j32
+
+cd $dir_name
+
+#Instrument driver and libtiff
+sed -i '33i // KLEE' src/libtiff/tif_dirread.c
+sed -i '34i #include <klee/klee.h>' src/libtiff/tif_dirread.c
+sed -i '35i #ifndef TRIDENT_OUTPUT' src/libtiff/tif_dirread.c
+sed -i '36i #define TRIDENT_OUTPUT(id, typestr, value) value' src/libtiff/tif_dirread.c
+sed -i '37i #endif' src/libtiff/tif_dirread.c
+
+sed -i '594i \\t} else if (__trident_choice("978", "bool", (int[]){td->td_nstrips, td->td_compression}, (char*[]){"x", "y"}, 2, (int*[]){}, (char*[]){}, 0)' src/libtiff/tif_dirread.c
+sed -i '595d' src/libtiff/tif_dirread.c
+
+sed -i '602i \\tTRIDENT_OUTPUT("obs", "i32", td->td_nstrips);' src/libtiff/tif_dirread.c
+sed -i '603i \\tklee_assert(td->td_nstrips != 2);' src/libtiff/tif_dirread.c
+
+## Compile instrumentation and test driver.
+cd src
+make CXX=$TRIDENT_CXX CC=$TRIDENT_CC CFLAGS="-ltrident_proxy -L/concolic-repair/lib -lkleeRuntest -I/klee/source/include -g -O0" -j32
+cd tools
+extract-bc tiffcp
+
+## Copy remaining files to run CPR.
+cd $script_dir
+cp repair.conf $dir_name
+cp spec.smt2 $dir_name
+cp test-input $dir_name
+cp -rf components $dir_name
+cp -rf test-input-files $dir_name
+cp -rf test-expected-output $dir_name
+cp -rf seed-dir $dir_name
