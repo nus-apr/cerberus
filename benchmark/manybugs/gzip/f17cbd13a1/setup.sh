941e9c4f620d039802b3d8566ac6ff3487f97cb5
rshariffdeen <rshariffdeen@gmail.com>
2021-06-29 17:50:29 +0800

    update test case

--- benchmark/manybugs/gzip/f17cbd13a1/setup.sh
+++ benchmark/manybugs/gzip/f17cbd13a1/setup.sh
@@ -1,8 +1,11 @@
-project_name=gzip
-bug_id=f17cbd13a1
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+fix_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=/data/$benchmark_name/$project_name/$fix_id
 scenario_id=gzip-bug-2009-09-26-a1d3d4019d-f17cbd13a1
 diff_file=gzip.c-a1d3d4019d
-dir_name=$1/manybugs/$project_name/$bug_id
+bug_id=$(echo $scenario_id | rev | cut -d "-" -f 2 | rev)
 download_url=https://repairbenchmarks.cs.umass.edu/ManyBugs/scenarios/${scenario_id}.tar.gz
 current_dir=$PWD
 mkdir -p $dir_name
@@ -22,7 +25,7 @@ rm -rf  coverage* \
         sanity \
         compile.pl \
         *~ \
-        test \
+        tests \
         reconfigure \
         preprocessed \
         fixed-program.txt
@@ -32,22 +35,26 @@ mv fix-failures bug-info
 mv $project_name src
 cd $dir_name/src
 cp $dir_name/diffs/${diff_file} $dir_name/src/$(echo $diff_file| cut -d'-' -f 1)
-make distclean
+sed -i "s#_GL_WARN_ON_USE (gets,#//#g" lib/stdio.in.h
+sed -i "s#root/mountpoint-genprog/genprog-many-bugs/${scenario_id}/gzip#/data/manybugs/${project_name}/${fix_id}/src#g" tests/Makefile
+sed -i "s#\$abs_srcdir#/data/manybugs/${project_name}/${fix_id}/src/tests#g" tests/hufts
 chown -R root $dir_name
 
-# Compile gzip.
-make clean
-CC=wllvm CXX=wllvm++ ./configure CFLAGS='-g -O0'
-CC=wllvm CXX=wllvm++ make CFLAGS="-g -O0 -static" -j32
+cd $dir_name
+## fix the test harness and the configuration script
+sed -i "s#/root/mountpoint-genprog/genprog-many-bugs/${scenario_id}#/data/manybugs/${project_name}/${fix_id}#g" test.sh
+sed -i "s#/data/manybugs/${project_name}/${fix_id}/limit#timeout 5#g" test.sh
+sed -i "s#/usr/bin/perl#perl#g" test.sh
+sed -i "s#cd ${project_name}#cd src#g" test.sh
 
-# Run Test Suite
-cd $dir_name/src/tests
-make helin-segv.log
-make help-version.log
-make hufts.log
-make mixed.log
-make memcpy-abuse.log
-make null-suffix-clobber.log
-make stdin.log
-make trailing-nul.log
-make zdiff.log
+#sed -i "29d" test.sh
+sed -i "6d" gzip-run-tests.pl
+sed -i "s#run_test 3 #run_test 2 #g" test.sh
+sed -i "s#run_test 4 #run_test 3 #g" test.sh
+sed -i "s#run_test 7 #run_test 6 #g" test.sh
+
+
+# Prophet requires/works on git source
+repo_url=git://git.savannah.gnu.org/gzip.git
+git clone $repo_url src-git
+cd src-git; git checkout $bug_id
