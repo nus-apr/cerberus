7c26c5d253af76021c9f350cf4b168e40cb2d0ff
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-07-02 00:35:04 +0800

    update php scripts

--- benchmark/manybugs/php/b84967d3e2/test.sh
+++ benchmark/manybugs/php/b84967d3e2/test.sh
@@ -3,7 +3,7 @@ benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=/data/$benchmark_name/$project_name/$bug_id
-scenario_id=php-bug-2011-02-27-e65d361fde-1d984a7ffd
+scenario_id=php-bug-2011-02-11-f912a2d087-b84967d3e2
 cd $dir_name
 TEST_ID=$1
 POS_N=2
