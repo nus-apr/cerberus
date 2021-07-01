caf974f10b6b8753775bee460ea9b9fe1c53f5cc
rshariffdeen <rshariffdeen@gmail.com>
2021-07-01 18:20:22 +0800

    update test case

---
+++ benchmark/manybugs/php/1d984a7ffd/config.sh
@@ -0,0 +1,26 @@
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=/data/$benchmark_name/$project_name/$bug_id
+cd $dir_name/src/php
+
+# Config libtiff.
+make clean
+./configure \
+  --enable-cli \
+  --disable-dom \
+  --disable-libxml  \
+  --disable-xml \
+  --disable-simplexml \
+  --disable-xmlreader  \
+  --disable-xmlwriter  \
+  --disable-pear  \
+  --disable-phar \
+  --disable-inline-optimization  \
+  --without-pcre-dir  \
+  --disable-fileinfo \
+  --disable-shared
+
+
+
--- benchmark/manybugs/php/1d984a7ffd/setup.sh
+++ benchmark/manybugs/php/1d984a7ffd/setup.sh
@@ -1,8 +1,11 @@
-project_name=php
-bug_id=1d984a7ffd
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+fix_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=/data/$benchmark_name/$project_name/$fix_id
 scenario_id=php-bug-2011-02-27-e65d361fde-1d984a7ffd
 diff_file=ext/tokenizer/tokenizer.c-e65d361fde
-dir_name=$1/manybugs/$project_name/$bug_id
+bug_id=$(echo $scenario_id | rev | cut -d "-" -f 2 | rev)
 download_url=https://repairbenchmarks.cs.umass.edu/ManyBugs/scenarios/${scenario_id}.tar.gz
 current_dir=$PWD
 mkdir -p $dir_name
@@ -32,14 +35,28 @@ mv fix-failures bug-info
 mv $project_name src
 cd $dir_name/src
 cp $dir_name/diffs/${diff_file} $dir_name/src/$(echo $diff_file| cut -d'-' -f 1)
-make distclean
 chown -R root $dir_name
+echo -ne 'all:\nclean:\ndistclean:\n' >> contrib/Makefile
+
+# Prophet requires/works on git source
+cd $dir_name
+hg clone http://hg.python.org/cpython src-hg
+cd src-hg; hg update $bug_id
+
+cd $dir_name
+## fix the test harness and the configuration script
+sed -i "s/cd python/cd src/" test.sh
+sed -i "s#/root/mountpoint-genprog/genprog-many-bugs/${scenario_id}#/data/manybugs/libtiff/${bug_id}#g" test.sh
+sed -i "s#/data/manybugs/libtiff/${bug_id}/limit#timeout 5#g" test.sh
+sed -i "s#/usr/bin/perl#perl#g" test.sh
+sed -i "s#cd libtiff#cd src#g" test.sh
+sed -i "s#&> /dev/null##" python-run-tests.pl
 
 
 # COMPILE PHP
 cd src/php
 make clean
-CC=wllvm CXX=wllvm++  ./configure \
+./configure \
   --enable-cli \
   --disable-dom \
   --disable-libxml  \
