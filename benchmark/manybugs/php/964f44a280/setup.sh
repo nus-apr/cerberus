c00d1c606d85e39bfe4ff63fc88fb471d2d05351
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-07-02 00:24:28 +0800

    update php scripts

--- benchmark/manybugs/php/964f44a280/setup.sh
+++ benchmark/manybugs/php/964f44a280/setup.sh
@@ -1,8 +1,11 @@
-project_name=php
-bug_id=964f44a280
-scenario_id=php-bug-2011-05-21-f5a9e17f9c-964f44a280
-diff_file=ext/pcre/php_pcre.c-f5a9e17f9c
-dir_name=$1/manybugs/$project_name/$bug_id
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+fix_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=/data/$benchmark_name/$project_name/$fix_id
+scenario_id=php-bug-2011-02-27-e65d361fde-1d984a7ffd
+diff_file=ext/tokenizer/tokenizer.c-e65d361fde
+bug_id=$(echo $scenario_id | rev | cut -d "-" -f 2 | rev)
 download_url=https://repairbenchmarks.cs.umass.edu/ManyBugs/scenarios/${scenario_id}.tar.gz
 current_dir=$PWD
 mkdir -p $dir_name
@@ -13,46 +16,49 @@ mv ${scenario_id} src
 rm ${scenario_id}.tar.gz
 mv src/* .
 rm -rf src
+
 rm -rf  coverage* \
         configuration-oracle \
         local-root \
         limit* \
+        php-tests-* \
         *.cache \
         *.debug.* \
-        sanity \
-        compile.pl \
         *~ \
-        test \
-        reconfigure \
-        preprocessed \
-        fixed-program.txt
-mv bugged-program.txt manifest.txt
-mv *.lines bug-info
-mv fix-failures bug-info
+        php.tar.gz && \
+    mv test.sh test.sh.orig && \
+    mv bug-failures bug-info && \
+    cp bugged-program.txt manifest.txt && \
+    mv *.lines bug-info && \
+    mv fix-failures bug-info
+
+chown -R root $dir_name
+grep -o -P '(?<=")[^"]+.phpt(?=")' php-run-tests.c > tests.all.txt
+grep -o -P '\d+(?= &&)' test.sh.orig > tests.indices.txt
+rm -f php-run-tests.c test.sh.orig
 mv $project_name src
 cd $dir_name/src
-cp $dir_name/diffs/${diff_file} $dir_name/src/$(echo $diff_file| cut -d'-' -f 1)
-make distclean
-chown -R root $dir_name
+make distclean &> /dev/null
+cp /experiments/benchmark/$benchmark_name/$project_name/base/* $dir_name
+# apply libxml fix
+cat ../libxml.patch | patch -p0
+
+#./configure CFLAGS="-save-temps=obj"
+#make -j`nproc`
+#cp $dir_name/src/$(echo $diff_file| cut -d'-' -f 1) $dir_name/preprocessed/$(echo $diff_file| cut -d'-' -f 1)
+#make distclean
+./configure && make -j`nproc`
 
 
-# COMPILE PHP
-cd src/php
-make clean
-CC=wllvm CXX=wllvm++  ./configure \
-  --enable-cli \
-  --disable-dom \
-  --disable-libxml  \
-  --disable-xml \
-  --disable-simplexml \
-  --disable-xmlreader  \
-  --disable-xmlwriter  \
-  --disable-pear  \
-  --disable-phar \
-  --disable-inline-optimization  \
-  --without-pcre-dir  \
-  --disable-fileinfo \
-  --disable-shared
+cd $dir_name
+chmod +x tester.py test.sh
+./tester.py build && rm tests.all.txt tests.indices.txt
 
-CC=clang CXX=clang++ make  -j32
+cd $dir_name/src
+find . -name tests.tar.gz -delete && find . -name tests -type d | tar -czf all-tests.tar.gz --files-from -
+find . -name tests -type d | rm -rf - && \
+    tar -xf all-tests.tar.gz && \
+    rm -f all-tests.tar.gz
+
+
 
