e71dba1516f8f4694a473df4e7cc1437653942ac
rshariffdeen <rshariffdeen@gmail.com>
2021-07-01 14:24:26 +0800

    update test case

--- benchmark/manybugs/fbc/5557/setup.sh
+++ benchmark/manybugs/fbc/5557/setup.sh
@@ -3,10 +3,8 @@ benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=/data/$benchmark_name/$project_name/$bug_id
-
 scenario_id=fbc-bug-5556-5557
 diff_file=src/rtlib/libfb_qb_str_convto_lng.c-5556
-
 download_url=https://repairbenchmarks.cs.umass.edu/ManyBugs/scenarios/${scenario_id}.tar.gz
 current_dir=$PWD
 mkdir -p $dir_name
--- benchmark/manybugs/fbc/5557/test.sh
+++ benchmark/manybugs/fbc/5557/test.sh
@@ -1,40 +1,41 @@
-project_name=fbc
-bug_id=5557
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=/data/$benchmark_name/$project_name/$bug_id
 scenario_id=fbc-bug-5556-5557
-diff_file=src/rtlib/libfb_qb_str_convto_lng.c-5556
-dir_name=$1/manybugs/$project_name/$bug_id
-download_url=https://repairbenchmarks.cs.umass.edu/ManyBugs/scenarios/${scenario_id}.tar.gz
-current_dir=$PWD
-mkdir -p $dir_name
 cd $dir_name
-wget $download_url
-tar xf ${scenario_id}.tar.gz
-mv ${scenario_id} src
-rm ${scenario_id}.tar.gz
-mv src/* .
-rm -rf src
-rm -rf  coverage* \
-        configuration-oracle \
-        local-root \
-        limit* \
-        *.cache \
-        *.debug.* \
-        sanity \
-        compile.pl \
-        *~ \
-        test \
-        reconfigure \
-        preprocessed \
-        fixed-program.txt
-mv bugged-program.txt manifest.txt
-mv *.lines bug-info
-mv fix-failures bug-info
-mv $project_name src
-cd $dir_name/src
-cp $dir_name/diffs/${diff_file} $dir_name/src/$(echo $diff_file| cut -d'-' -f 1)
-make distclean
-chown -R root $dir_name
-
-# COMPILE FBC
+TEST_ID=$1
+POS_N=2
+NEG_N=1
 
 
+if [ -z "$TEST_ID" ]
+then
+   # Run passing test cases
+  for i in `seq -s " " -f "p%g"  1 $POS_N`
+  do
+  bash test.sh $i /data
+  done
+
+  # Run failing test cases
+  for i in `seq -s " " -f "n%g"  1 $NEG_N`
+  do
+  bash test.sh $i /data
+  done
+else
+  pattern=`expr substr "$TEST_ID" 1 1`
+  num=`expr substr "$TEST_ID" 2 ${#TEST_ID}`
+
+  if [[ $pattern == 'n' ]]; then
+      cd $dir_name
+      timeout 5 bash test.sh $TEST_ID
+  elif [[ $pattern == 'p' ]]; then
+      cd $dir_name
+      timeout 5 bash test.sh $TEST_ID
+  else
+      cd $dir_name/src
+      timeout 5 perl $dir_name/${project_name}-run-tests.pl $TEST_ID
+  fi
+
+fi
