d029ae5001a14d33cf80fd228cf7428f79213bb0
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-07-07 17:46:41 +0800

    update genprog instrumentation in gzip

--- benchmark/manybugs/gzip/f17cbd13a1/genprog/instrument.sh
+++ benchmark/manybugs/gzip/f17cbd13a1/genprog/instrument.sh
@@ -8,7 +8,6 @@ cd $dir_name/src
 make clean
 make CC="cilly --save-temps -std=c99 -fno-optimize-sibling-calls -fno-strict-aliasing -fno-asm" -j`nproc`
 
-cp $script_dir/compile.pl $dir_name/src
 cp $dir_name/manifest.txt $dir_name/src/bugged-program.txt
 cfile=$(head -n 1 $dir_name/manifest.txt)
 cilfile=$(echo $(echo $cfile | cut -d$"." -f1).cil.c)
@@ -23,4 +22,4 @@ rm -rf repair.cache
 rm -rf repair.debug.*
 
 cp $dir_name/compile.pl $dir_name/src
-sed -i 's#/project = "python"#project = "${$dir_name/src}"#g' $dir_name/src/compile.pl
+sed -i 's#/project = "gzip"#project = "${$dir_name/src}"#g' $dir_name/src/compile.pl
