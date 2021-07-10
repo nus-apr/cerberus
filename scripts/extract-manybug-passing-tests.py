6afd1a5f39eec6d227f35ffa6705cac5b869ae34
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-07-11 01:36:24 +0800

    update script to collect passing ids

--- scripts/extract-manybug-passing-tests.py
+++ scripts/extract-manybug-passing-tests.py
@@ -2,8 +2,14 @@ import os
 import json
 
 directory = "/experiments/benchmark/manybugs"
-subject_list = [x[0] for x in os.walk(directory)]
+sub_dir_list = [x[0] for x in os.walk(directory)]
 result_list = dict()
+subject_list = []
+
+for dir_path in sub_dir_list[1:]:
+    subject_name = dir_path.split("/")[4]
+    subject_list.append(subject_name)
+subject_list = list(set(subject_list))
 
 
 def write_as_json(data_list, output_file_path):
@@ -13,7 +19,12 @@ def write_as_json(data_list, output_file_path):
 
 
 for subject in subject_list:
-    id_list = subject_list = [x[0] for x in os.walk(directory + "/" + subject)]
+    sub_dir_list = [x[0] for x in os.walk(directory + "/" + subject)]
+    id_list = []
+    for dir_path in sub_dir_list[1:]:
+        bug_id = dir_path.split("/")[5]
+        id_list.append(bug_id)
+    id_list = list(set(id_list))
     for bug_id in id_list:
         exp_dir = "/experiments/benchmark/manybugs/" + subject + "/" + bug_id
         data_dir = "/data/manybugs/" + subject + "/" + bug_id
@@ -23,16 +34,16 @@ for subject in subject_list:
         if os.path.isfile(test_all):
             pass_list = []
             with open(test_all, "r") as t_file:
-                test_list = t_file.readlines()
+                test_list = [x.strip().replace("\n", "") for x in t_file.readlines()]
 
             if os.path.isfile(test_log):
                 with open(test_log) as log_file:
                     log_lines = log_file.readlines()
                     test_case = None
                     test_status = None
-                    for line in log_lines[1:]:
+                    for line in log_lines:
                         if test_case is None:
-                            test_case = line.strip().replace("\n", "")
+                            test_case = line.split(": ")[1].strip().replace("\n", "")
                         else:
                             test_status = line
 
