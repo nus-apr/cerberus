423af243c5ac35849f634c084a1fa44a4051305c
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-07-13 05:14:16 +0800

    update prophet instrumentation

--- benchmark/manybugs/php/1d984a7ffd/prophet/instrument.sh
+++ benchmark/manybugs/php/1d984a7ffd/prophet/instrument.sh
@@ -6,12 +6,7 @@ dir_name=/data/$benchmark_name/$project_name/$fix_id
 
 mkdir $dir_name/prophet
 
-cat <<EOF > $dir_name/prophet/build
-export PHP_AUTOHEADER=/deps/php/autoconf-2.13-build/bin/autoheader PHP_AUTOCONF=/deps/php/autoconf-2.13-build/bin/autoconf  PATH=/deps/php/bison-2.2-build/bin:$PATH
-/prophet-gpl/tools/php-build.py "\$@"
-EOF
 
-chmod +x $dir_name/prophet/build
 
 cat <<EOF > $dir_name/prophet/prophet.conf
 revision_file=/data/$benchmark_name/$project_name/$fix_id/prophet/prophet.revlog
@@ -19,7 +14,7 @@ src_dir=/data/$benchmark_name/$project_name/$fix_id/src
 test_dir=/data/$benchmark_name/$project_name/$fix_id/tests
 bugged_file=ext/tokenizer/tokenizer.c
 fixed_out_file=$project_name-fix-$fix_id.c
-build_cmd=$dir_name/prophet/build
+build_cmd=/prophet-gpl/tools/$project_name-build.py
 test_cmd=/prophet-gpl/tools/$project_name-test.py
 dep_dir=/prophet-gpl/benchmarks/$project_name-deps
 localizer=profile
--- benchmark/manybugs/php/d4f05fbffc/prophet/instrument.sh
+++ benchmark/manybugs/php/d4f05fbffc/prophet/instrument.sh
@@ -6,12 +6,7 @@ dir_name=/data/$benchmark_name/$project_name/$fix_id
 
 mkdir $dir_name/prophet
 
-cat <<EOF > $dir_name/prophet/build
-export PHP_AUTOHEADER=/deps/php/autoconf-2.13-build/bin/autoheader PHP_AUTOCONF=/deps/php/autoconf-2.13-build/bin/autoconf  PATH=/deps/php/bison-2.2-build/bin:$PATH
-/prophet-gpl/tools/php-build.py "\$@"
-EOF
 
-chmod +x $dir_name/prophet/build
 
 export PHP_AUTOHEADER=/deps/php/autoconf-2.13-build/bin/autoheader PHP_AUTOCONF=/deps/php/autoconf-2.13-build/bin/autoconf
 export PATH=/deps/php/bison-2.2-build/bin:$PATH
@@ -22,7 +17,7 @@ src_dir=/data/$benchmark_name/$project_name/$fix_id/src
 test_dir=/data/$benchmark_name/$project_name/$fix_id/tests
 bugged_file=Zend/zend_exceptions.c
 fixed_out_file=$project_name-fix-$fix_id.c
-build_cmd=$dir_name/prophet/build
+build_cmd=/prophet-gpl/tools/$project_name-build.py
 test_cmd=/prophet-gpl/tools/$project_name-test.py
 dep_dir=/prophet-gpl/benchmarks/$project_name-deps
 localizer=profile
