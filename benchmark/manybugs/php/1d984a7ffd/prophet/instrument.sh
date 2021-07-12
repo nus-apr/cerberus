df7af8afc7dda368400202e6ce868cbd032dee3e
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-07-13 00:59:52 +0800

    update prophet instrumentation

--- benchmark/manybugs/php/1d984a7ffd/prophet/instrument.sh
+++ benchmark/manybugs/php/1d984a7ffd/prophet/instrument.sh
@@ -6,8 +6,12 @@ dir_name=/data/$benchmark_name/$project_name/$fix_id
 
 mkdir $dir_name/prophet
 
-export PHP_AUTOHEADER=/deps/php/autoconf-2.13-build/bin/autoheader PHP_AUTOCONF=/deps/php/autoconf-2.13-build/bin/autoconf
-export PATH=/deps/php/bison-2.2-build/bin:$PATH
+cat <<EOF > $dir_name/prophet/build
+export PHP_AUTOHEADER=/deps/php/autoconf-2.13-build/bin/autoheader PHP_AUTOCONF=/deps/php/autoconf-2.13-build/bin/autoconf  PATH=/deps/php/bison-2.2-build/bin:$PATH
+/prophet-gpl/tools/php-build.py $@
+EOF
+
+chmod +x $dir_name/prophet/build
 
 cat <<EOF > $dir_name/prophet/prophet.conf
 revision_file=/data/$benchmark_name/$project_name/$fix_id/prophet/prophet.revlog
@@ -15,7 +19,7 @@ src_dir=/data/$benchmark_name/$project_name/$fix_id/src
 test_dir=/data/$benchmark_name/$project_name/$fix_id/tests
 bugged_file=ext/tokenizer/tokenizer.c
 fixed_out_file=$project_name-fix-$fix_id.c
-build_cmd=/prophet-gpl/tools/$project_name-build.py
+build_cmd=$dir_name/prophet/build
 test_cmd=/prophet-gpl/tools/$project_name-test.py
 dep_dir=/prophet-gpl/benchmarks/$project_name-deps
 localizer=profile
--- benchmark/manybugs/php/efc94f3115/prophet/instrument.sh
+++ benchmark/manybugs/php/efc94f3115/prophet/instrument.sh
@@ -6,6 +6,13 @@ dir_name=/data/$benchmark_name/$project_name/$fix_id
 
 mkdir $dir_name/prophet
 
+cat <<EOF > $dir_name/prophet/build
+export PHP_AUTOHEADER=/deps/php/autoconf-2.13-build/bin/autoheader PHP_AUTOCONF=/deps/php/autoconf-2.13-build/bin/autoconf  PATH=/deps/php/bison-2.2-build/bin:$PATH
+/prophet-gpl/tools/php-build.py $@
+EOF
+
+chmod +x $dir_name/prophet/build
+
 export PHP_AUTOHEADER=/deps/php/autoconf-2.13-build/bin/autoheader PHP_AUTOCONF=/deps/php/autoconf-2.13-build/bin/autoconf
 export PATH=/deps/php/bison-2.2-build/bin:$PATH
 
@@ -15,7 +22,7 @@ src_dir=/data/$benchmark_name/$project_name/$fix_id/src
 test_dir=/data/$benchmark_name/$project_name/$fix_id/tests
 bugged_file=ext/standard/html.c
 fixed_out_file=$project_name-fix-$fix_id.c
-build_cmd=/prophet-gpl/tools/$project_name-build.py
+build_cmd=$dir_name/prophet/build
 test_cmd=/prophet-gpl/tools/$project_name-test.py
 dep_dir=/prophet-gpl/benchmarks/$project_name-deps
 localizer=profile
