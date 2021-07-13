6afc30f5c5f38ab447e04d8a27ee520039c7dec0
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-07-13 20:43:25 +0800

    update cpr instrumentation for libtiff

--- benchmark/manybugs/libtiff/0a36d7f/cpr/instrument.sh
+++ benchmark/manybugs/libtiff/0a36d7f/cpr/instrument.sh
@@ -4,17 +4,6 @@ project_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 fix_id=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 dir_name=/data/$benchmark_name/$project_name/$fix_id
 
-cd $dir_name/src/test
-
-# Copy Seed Files
-mkdir $dir_name/seed-dir
-find . -type f -iname '*.tiff' -exec cp  {} $dir_name/seed-dir/ \;
-find . -type f -iname '*.bmp' -exec cp  {} $dir_name/seed-dir/ \;
-find . -type f -iname '*.gif' -exec cp  {} $dir_name/seed-dir/ \;
-find . -type f -iname '*.pgm' -exec cp  {} $dir_name/seed-dir/ \;
-find . -type f -iname '*.ppm' -exec cp  {} $dir_name/seed-dir/ \;
-find . -type f -iname '*.pbm' -exec cp  {} $dir_name/seed-dir/ \;
-
 cd $dir_name/src
 make clean
 
@@ -85,17 +74,6 @@ cd ./test
 make CXX=$TRIDENT_CXX CC=$TRIDENT_CC CFLAGS="-L/CPR/lib -ltrident_proxy -L/klee/build/lib  -lkleeRuntest -I/klee/source/include -g -O0" -j32 long_tag.log short_tag.log ascii_tag.log strip_rw.log
 extract-bc long_tag
 
-# Copy remaining files to run CPR.
-cp $script_dir/repair.conf $dir_name
-cp $script_dir/spec.smt2 $dir_name
-cp $script_dir/test-input $dir_name
-cp $script_dir/seed-file $dir_name
-cp -rf $script_dir/components $dir_name
-cp -rf $script_dir/test-input-files $dir_name
-cp -rf $script_dir/test-expected-output $dir_name
-cp $script_dir/test-config.json $dir_name
-cp $script_dir/seed-config.json $dir_name
-cp $script_dir/seed-dir/* $dir_name/seed-dir
 
 # Convert shell to binary as a driver
 cd $dir_name/src/test
@@ -105,3 +83,67 @@ CC=$TRIDENT_CC CXX=$TRIDENT_CXX shc -f tiffcp-split.sh
 CC=$TRIDENT_CC CXX=$TRIDENT_CXX shc -f tiffcp-split-join.sh
 mv tiffcp-split.sh.x tiffcp-split
 mv tiffcp-split-join.sh.x tiffcp-split-join
+
+
+cat <<EOF > $dir_name/cpr/repair.conf
+project_path:/data/$benchmark_name/$project_name/$fix_id
+tag_id:$fix_id
+src_directory:src
+config_command:skip
+build_command:skip
+custom_comp_list:cpr/components/x.smt2,cpr/components/y.smt2,cpr/components/z.smt2
+general_comp_list:equal.smt2,not-equal.smt2,less-than.smt2,less-or-equal.smt2,division.smt2
+depth:3
+loc_patch:/data/$benchmark_name/$project_name/$fix_id/src/libtiff/tif_dirread.c:976
+loc_bug:/data/$benchmark_name/$project_name/$fix_id/src/libtiff/tif_unix.c:192
+gen_limit:80
+stack_size:15000
+dist_metric:angelic
+spec_path:cpr/spec.smt2
+test_input_dir:cpr/test-input-files
+test_output_dir:cpr/test-expected-output
+seed_dir:cpr/seed-dir
+path_seed_suite:cpr/seed-config.json
+path_test_suite:cpr/test-config.json
+EOF
+
+
+# Create patch components
+mkdir $dir_name/cpr/components
+declare -a arr_var=("x" "y" "z")
+#declare -a arr_const=("constant_a")
+# Create components for program variables
+for i in "${arr_var[@]}"
+do
+cat <<EOF > $dir_name/cpr/components/$i.smt2
+(declare-const rvalue_$i (_ BitVec 32))
+(declare-const lvalue_$i (_ BitVec 32))
+(declare-const rreturn (_ BitVec 32))
+(declare-const lreturn (_ BitVec 32))
+(assert (and (= rreturn rvalue_$i) (= lreturn lvalue_$i)))
+EOF
+done
+
+# Create components for constants
+#for i in "${arr_const[@]}"
+#do
+#cp /CPR/components/$i.smt2 $dir_name/cpr/components
+#done
+
+
+# Copy Seed Files
+cd $dir_name/src/test
+mkdir $dir_name/cpr/seed-dir
+find . -type f -iname '*.tiff' -exec cp  {} $dir_name/cpr/seed-dir/ \;
+find . -type f -iname '*.bmp' -exec cp  {} $dir_name/cpr/seed-dir/ \;
+find . -type f -iname '*.gif' -exec cp  {} $dir_name/cpr/seed-dir/ \;
+find . -type f -iname '*.pgm' -exec cp  {} $dir_name/cpr/seed-dir/ \;
+find . -type f -iname '*.ppm' -exec cp  {} $dir_name/cpr/seed-dir/ \;
+find . -type f -iname '*.pbm' -exec cp  {} $dir_name/cpr/seed-dir/ \;
+
+# Copy remaining files to run CPR.
+cp $script_dir/spec.smt2 $dir_name/cpr
+cp -rf $script_dir/test-input-files $dir_name/cpr
+cp -rf $script_dir/test-expected-output $dir_name/cpr
+cp $script_dir/test-config.json $dir_name/cpr
+cp $script_dir/seed-config.json $dir_name/cpr
