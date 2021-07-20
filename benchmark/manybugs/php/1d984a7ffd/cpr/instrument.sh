0ba90e3a6cdbbfd664f6d413c7b5ce1868ecd40e
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-07-20 23:43:24 +0800

    update php instrumentation for cpr

--- benchmark/manybugs/php/1d984a7ffd/cpr/instrument.sh
+++ benchmark/manybugs/php/1d984a7ffd/cpr/instrument.sh
@@ -193,6 +193,7 @@ test_input_file:cpr/test-input
 test_output_list:cpr/expected-output/t1.smt2,cpr/expected-output/t2.smt2
 mask_arg:$(seq -s "," -f "%g" 0 52)
 preserve_bc:true
+timeout_klee:1800
 generalize_seed_input:-d output_handler= -d open_basedir= -d safe_mode=0 -d disable_functions= -d output_buffering=Off -d error_reporting=32767 -d display_errors=1 -d display_startup_errors=1 -d log_errors=0 -d html_errors=0 -d track_errors=1 -d report_memleaks=1 -d report_zend_debug=0 -d docref_root= -d docref_ext=.html -d error_prepend_string= -d error_append_string= -d auto_prepend_file= -d auto_append_file= -d magic_quotes_runtime=0 -d ignore_repeated_errors=0 -d precision=14 -d unicode.runtime_encoding=ISO-8859-1 -d unicode.script_encoding=UTF-8 -d unicode.output_encoding=UTF-8 -d unicode.from_error_mode=U_INVALID_SUBSTITUTE -f \$POC
 generalize_test_input:-d output_handler= -d open_basedir= -d safe_mode=0 -d disable_functions= -d output_buffering=Off -d error_reporting=32767 -d display_errors=1 -d display_startup_errors=1 -d log_errors=0 -d html_errors=0 -d track_errors=1 -d report_memleaks=1 -d report_zend_debug=0 -d docref_root= -d docref_ext=.html -d error_prepend_string= -d error_append_string= -d auto_prepend_file= -d auto_append_file= -d magic_quotes_runtime=0 -d ignore_repeated_errors=0 -d precision=14 -d unicode.runtime_encoding=ISO-8859-1 -d unicode.script_encoding=UTF-8 -d unicode.output_encoding=UTF-8 -d unicode.from_error_mode=U_INVALID_SUBSTITUTE -f \$POC
 EOF
