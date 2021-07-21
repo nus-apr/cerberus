2c180fd100730d2bfe3711ee243cb1aa0a46bf4c
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-07-21 08:27:36 +0800

    update configuration on php subject to use autoconf 2.13

--- benchmark/manybugs/php/efcb9a71cd/config.sh
+++ benchmark/manybugs/php/efcb9a71cd/config.sh
@@ -4,11 +4,11 @@ project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=/data/$benchmark_name/$project_name/$bug_id
 cd $dir_name/src
-
-# Config libtiff.
-make clean
+export PHP_AUTOHEADER=/deps/php/autoconf-2.13-build/bin/autoheader PHP_AUTOCONF=/deps/php/autoconf-2.13-build/bin/autoconf
 PATH_ORIG=$PATH
 export PATH=/deps/php/bison-2.2-build/bin:$PATH_ORIG
+# Config libtiff.
+make clean
 ./configure \
   --enable-cli \
   --disable-dom \
