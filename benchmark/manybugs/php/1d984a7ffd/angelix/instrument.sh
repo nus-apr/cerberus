7ebcddf988b0f9b6b7bcfd9b53c514d4ee469432
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-08-06 04:18:46 +0800

    update instrumentation

--- benchmark/manybugs/php/1d984a7ffd/angelix/instrument.sh
+++ benchmark/manybugs/php/1d984a7ffd/angelix/instrument.sh
@@ -9,7 +9,7 @@ dir_name=/data/$benchmark_name/$project_name/$fix_id
 version=308734-308761 #this is the angelix version
 gold_file=ext/tokenizer/tokenizer.c-$fix_id
 # buggy_file=ext/tokenizer/tokenizer.c-e65d361fde
-export ANGELIX_ARGS=" --defect if-conditions --synthesis-levels extended-arithmetic --klee-search dfs --klee-max-forks 200 --synthesis-timeout 200000 --group-size 1  "
+echo " --defect if-conditions --synthesis-levels extended-arithmetic --klee-search dfs --klee-max-forks 200 --synthesis-timeout 200000 --group-size 1  " > /tmp/ANGELIX_ARGS
 
 clean-source () {
     local directory="$1"
