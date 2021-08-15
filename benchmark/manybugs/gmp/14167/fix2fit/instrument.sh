101f04ed3b39bb4e3e2afb3ba272e01d52dd462b
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-08-15 22:56:56 +0800

    update gmp instrumentation

--- benchmark/manybugs/gmp/14167/fix2fit/instrument.sh
+++ benchmark/manybugs/gmp/14167/fix2fit/instrument.sh
@@ -3,10 +3,11 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 4 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 fix_id=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+exp_dir_name=/data/$benchmark_name/$project_name/$fix_id
 src_dir_name=/data/$benchmark_name/$project_name/$fix_id/src
 test_dir_name=/data/$benchmark_name/$project_name/$fix_id/tests
 target_dir=/experiments/benchmark/$benchmark_name/$project_name/$fix_id
-cd $test_dir_name
+cd $exp_dir_name
 sed -i 's/make/make -e/g' gmp-run-tests.pl
 # Create Seed Files
 mkdir $target_dir/seed-dir
