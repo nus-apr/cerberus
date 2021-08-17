ec409231e5b8fbeb9c4c048d031502c0995140fa
rshariffdeen <rshariffdeen@gmail.com>
2021-08-17 14:37:57 +0800

    detect if compilation failed

--- app/tools/Fix2Fit.py
+++ app/tools/Fix2Fit.py
@@ -119,6 +119,7 @@ class Fix2Fit(AbstractTool):
                     elif "no negative tests" in line:
                         emitter.warning("\t\t\t\t[warning] no negative tests")
                     elif "failed to infer compile commands" in line:
+                        is_error = True
                         emitter.error("\t\t\t\t[error] compilation command not found")
         if is_error:
             emitter.error("\t\t\t\t[error] error detected in logs")
