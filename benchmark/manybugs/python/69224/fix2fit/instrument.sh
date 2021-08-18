e95ab41daf17b1bdb297d454b18764e0bafa4cc8
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-08-18 23:30:28 +0800

    update test-case ids

--- benchmark/manybugs/python/69224/fix2fit/instrument.sh
+++ benchmark/manybugs/python/69224/fix2fit/instrument.sh
@@ -15,8 +15,8 @@ find . -type f -iname 'test_*.py' -exec cp  {} $target_dir/seed-dir/ \;
 
 cd $exp_dir_name
 sed -i "11d" python-run-tests.pl
-#sed -i "s/run_test 243/run_test 244/" test.sh
-#sed -i "s/n1\) run_test 244/n1\) run_test 243/" test.sh
+sed -i "s/run_test 243/run_test 244/" test.sh
+sed -i "s/n1\) run_test 244/n1\) run_test 243/" test.sh
 make clean
 
 if [ ! -f "$src_dir_name/INSTRUMENTED_FIX2FIT" ]; then
