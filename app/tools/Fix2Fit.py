45485d57401a912e7dbe3f7d28b65a9d12241371
rshariffdeen <rshariffdeen@gmail.com>
2021-08-23 20:39:36 +0800

    fix list ref

--- app/tools/Fix2Fit.py
+++ app/tools/Fix2Fit.py
@@ -202,13 +202,13 @@ class Fix2Fit(AbstractTool):
                         emitter.warning("\t\t\t\t[warning] no patch found by F1X")
                     elif "negative tests: [" in line:
                         reported_failing_test = str(line).split("negative tests: [")[-1].split("]")[0].split(", ")
+                    elif "search space size: " in line:
+                        size_search_space = line.split("search space size: ")[-1].strip()
         with open(self.log_output_path, "r") as log_file:
             log_lines = log_file.readlines()
             for line in log_lines:
                 if "candidates evaluated: " in line:
                     count_enumerations = int(line.split("candidates evaluated: ")[-1].strip())
-                elif "search space size: " in line:
-                    size_search_space = line.split("search space size: ")[-1].strip()
                 elif "plausible patches: " in line:
                     count_plausible = int(line.split("plausible patches: ")[-1].strip())
                 elif "patches successfully generated" in line:
