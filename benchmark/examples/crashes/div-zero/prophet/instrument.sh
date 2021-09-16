f440a97bea9fc595c7c6556e8add93b878500822
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-09-17 02:36:53 +0800

    add empty test directory for prophet init

--- benchmark/examples/crashes/div-zero/prophet/instrument.sh
+++ benchmark/examples/crashes/div-zero/prophet/instrument.sh
@@ -13,6 +13,7 @@ cp $script_dir/build.py $dir_name/prophet
 cat <<EOF > $dir_name/prophet/prophet.conf
 revision_file=/experiment/$benchmark_name/$project_name/$bug_id/prophet/prophet.revlog
 src_dir=/experiment/$benchmark_name/$project_name/$bug_id/src
+test_dir=/experiment/$benchmark_name/$project_name/$bug_id/tests
 bugged_file=test.c
 fixed_out_file=$project_name-fix-$bug_id.c
 build_cmd=$dir_name/prophet/build.py
