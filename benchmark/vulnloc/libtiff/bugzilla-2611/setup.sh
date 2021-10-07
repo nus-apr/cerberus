0712140cf4eeb022f7685d8001e79a1b0281ae8c
rshariffdeen <rshariffdeen@gmail.com>
2021-10-07 16:30:06 +0800

    add libtiff experiment subjects

---
+++ benchmark/vulnloc/libtiff/bugzilla-2611/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+fix_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=/experiment/$benchmark_name/$project_name/$fix_id
+current_dir=$PWD
+mkdir -p $dir_name
+cd $dir_name
+mkdir dev-patch
+
+project_url=https://github.com/vadz/libtiff.git
+fix_commit_id=43bc256d8ae44b92d2734a3c5bc73957a4d7c1ec
+bug_commit_id=9a72a69
+
+cd $dir_name
+git clone $project_url src
+cd src
+git checkout $bug_commit_id
+git format-patch -1 $fix_commit_id
+cp *.patch $dir_name/dev-patch
+
+./autogen.sh
---
+++ benchmark/vulnloc/libtiff/bugzilla-2633/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+fix_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=/experiment/$benchmark_name/$project_name/$fix_id
+current_dir=$PWD
+mkdir -p $dir_name
+cd $dir_name
+mkdir dev-patch
+
+project_url=https://github.com/vadz/libtiff.git
+fix_commit_id=5ed9fea523316c2f5cec4d393e4d5d671c2dbc33
+bug_commit_id=f3069a5
+
+cd $dir_name
+git clone $project_url src
+cd src
+git checkout $bug_commit_id
+git format-patch -1 $fix_commit_id
+cp *.patch $dir_name/dev-patch
+
+./autogen.sh
