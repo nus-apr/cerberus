0a0960d2666cd805d3a2e6f5dfa75e47b126b478
rshariffdeen <rshariffdeen@gmail.com>
2021-10-12 16:40:45 +0800

    update build/config scripts for coreutils in vulnloc

--- benchmark/vulnloc/coreutils/gnubug-19784/build.sh
+++ benchmark/vulnloc/coreutils/gnubug-19784/build.sh
@@ -5,4 +5,5 @@ project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=/experiment/$benchmark_name/$project_name/$bug_id
 cd $dir_name/src
-make CFLAGS="-fPIC -fPIE -g -O0" CXXFLAGS=$CFLAGS src/make-prime-list -j32
+make CFLAGS="-fsanitize=address -ggdb -fPIC -fPIE -g -O0" CXXFLAGS=$CFLAGS  LDFLAGS="-fsanitize=address" src/make-prime-list -j`nproc`
+
