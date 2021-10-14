9629538b1932067226fbb64a462851c05bee1191
rshariffdeen <rshariffdeen@gmail.com>
2021-10-14 14:30:29 +0800

    update prophet instrumentation for vulnloc libtiff subjects

--- benchmark/vulnloc/libtiff/CVE-2016-10092/setup.sh
+++ benchmark/vulnloc/libtiff/CVE-2016-10092/setup.sh
@@ -4,6 +4,7 @@ benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=/experiment/$benchmark_name/$project_name/$bug_id
+fix_file=$1
 current_dir=$PWD
 mkdir -p $dir_name
 cd $dir_name
--- benchmark/vulnloc/libtiff/CVE-2017-7595/build.sh
+++ benchmark/vulnloc/libtiff/CVE-2017-7595/build.sh
@@ -4,6 +4,7 @@ benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=/experiment/$benchmark_name/$project_name/$bug_id
+fix_file=$1
 cd $dir_name/src
 
 make CFLAGS="-g -O0 -static" -j32
