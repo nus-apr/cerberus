4269e3d07d3d6ec1b622357b90755e7f084f9495
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-11-09 18:58:43 +0800

    add auxiliary dir to volume

--- app/benchmarks/AbstractBenchmark.py
+++ app/benchmarks/AbstractBenchmark.py
@@ -71,11 +71,14 @@ class AbstractBenchmark:
                 shutil.rmtree(dir_exp_local)
             dir_output_local = definitions.DIR_ARTIFACTS + "/" + str(config_id) + "-" + self.name + "-" + \
                                tool_name + "-" + subject_name + "-" + bug_id
+            dir_aux_local = self.bench_dir_path + "/" + self.name + "/" + subject_name + "/.aux"
+            dir_aux_container = "/experiments/benchmark/" + self.name + "/" + subject_name + "/.aux"
             volume_list = {
                 # dir_exp_local: {'bind': '/experiment', 'mode': 'rw'},
                 self.log_dir_path: {'bind': '/logs', 'mode': 'rw'},
                 dir_output_local: {'bind': '/output', 'mode': 'rw'},
-                dir_setup_local: {'bind': dir_setup_container, 'mode': 'rw'}
+                dir_setup_local: {'bind': dir_setup_container, 'mode': 'rw'},
+                dir_aux_local: {'bind': dir_aux_container, 'mode': 'rw'}
             }
             container_id = container.get_container(tool_name, self.name, subject_name, bug_id, config_id)
             if container_id:
