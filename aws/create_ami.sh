5eb82d11897c9953284e36f50aa44458a114f50f
rshariffdeen <rshariffdeen@gmail.com>
2021-11-09 15:08:20 +0800

    update ami setup script

--- aws/create_ami.sh
+++ aws/create_ami.sh
@@ -3,3 +3,4 @@ sudo apt install docker.io
 sudo usermod -aG docker ubuntu
 sudo apt install pypy3 pypy3-dev
 pypy3 -m easy_install docker more_itertools
+git clone https://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:x-oauth-basic@github.com/rshariffdeen/cerberus
