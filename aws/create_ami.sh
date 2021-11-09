ef8c6a401b9b74804dc5756c5541f319fe306a42
rshariffdeen <rshariffdeen@gmail.com>
2021-11-09 15:16:17 +0800

    remove libgdb

--- aws/create_ami.sh
+++ aws/create_ami.sh
@@ -2,5 +2,5 @@ sudo apt update
 sudo apt install -y docker.io
 sudo usermod -aG docker ubuntu
 sudo apt install -y pypy3 pypy3-dev
-pypy3 -m easy_install docker more_itertools
+sudo pypy3 -m easy_install docker more_itertools
 git clone https://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:x-oauth-basic@github.com/rshariffdeen/cerberus
