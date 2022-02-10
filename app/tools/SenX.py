1cb6ea942afbaa59bd68df1ff16f39493013516e
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-02-10 16:57:45 +0800

    add new line break for timestamp to separate output of tool

--- app/tools/SenX.py
+++ app/tools/SenX.py
@@ -78,7 +78,7 @@ class SenX(AbstractTool):
             else:
                 emitter.success("\t\t\t[success] {0} ended successfully".format(self.name))
             emitter.highlight("\t\t\tlog file: {0}".format(self.log_output_path))
-            timestamp_command = "echo $(date '+%a %d %b %Y %H:%M:%S %p') >> " + self.log_output_path
+            timestamp_command = "printf '\n'; echo $(date '+%a %d %b %Y %H:%M:%S %p') >> " + self.log_output_path
             execute_command(timestamp_command)
 
         return
