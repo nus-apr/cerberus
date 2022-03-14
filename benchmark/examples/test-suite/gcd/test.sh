aea3d8b91949926489a279ee558280d425e24c58
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-03-14 22:11:13 +0800

    update test case

--- benchmark/examples/test-suite/gcd/test.sh
+++ benchmark/examples/test-suite/gcd/test.sh
@@ -4,10 +4,16 @@ benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=$1/$benchmark_name/$project_name/$bug_id
-cd $dir_name
-TEST_ID=$2
 POS_N=10
 NEG_N=1
+
+BINARY_PATH="$dir_name/src/gcd"
+TEST_ID=$2
+
+if [ -n "$3" ];
+then
+  BINARY_PATH=$3
+fi
 
 
 if [ -z "$TEST_ID" ]
@@ -15,23 +21,23 @@ then
    # Run passing test cases
   for i in `seq -s " " -f "p%g"  1 $POS_N`
   do
-  bash test.sh $1 $i
+  bash $script_dir/test.sh $1 $i $BINARY_PATH
   done
 
   # Run failing test cases
   for i in `seq -s " " -f "n%g"  1 $NEG_N`
   do
-  bash test.sh $1 $i
+  bash $script_dir/test.sh $1 $i $BINARY_PATH
   done
 else
   pattern=`expr substr "$TEST_ID" 1 1`
   num=`expr substr "$TEST_ID" 2 ${#TEST_ID}`
   if [[ $pattern == 'n' ]]; then
-      timeout 25 bash oracle-2 $1 $TEST_ID
+      timeout 25 bash $script_dir/oracle-2 $1 $TEST_ID $BINARY_PATH
   elif [[ $pattern == 'p' ]]; then
-      timeout 25 bash oracle-2 $1 $TEST_ID
+      timeout 25 bash $script_dir/oracle-2 $1 $TEST_ID $BINARY_PATH
   else
-      timeout 25 bash oracle-1 $1 $TEST_ID
+      timeout 25 bash $script_dir/oracle-1 $1 $TEST_ID $BINARY_PATH
   fi
 
 fi
