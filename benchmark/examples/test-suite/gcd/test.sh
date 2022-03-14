15eff22757b99943828498232e51915ffd2a65d7
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-03-14 15:43:31 +0800

    add example gcd

---
+++ benchmark/examples/test-suite/gcd/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=$1/$benchmark_name/$project_name/$bug_id
+cd $dir_name
+TEST_ID=$2
+POS_N=10
+NEG_N=1
+
+
+if [ -z "$TEST_ID" ]
+then
+   # Run passing test cases
+  for i in `seq -s " " -f "p%g"  1 $POS_N`
+  do
+  bash test.sh $1 $i
+  done
+
+  # Run failing test cases
+  for i in `seq -s " " -f "n%g"  1 $NEG_N`
+  do
+  bash test.sh $1 $i
+  done
+else
+  pattern=`expr substr "$TEST_ID" 1 1`
+  num=`expr substr "$TEST_ID" 2 ${#TEST_ID}`
+  if [[ $pattern == 'n' ]]; then
+      timeout 25 bash oracle-2 $1 $TEST_ID
+  elif [[ $pattern == 'p' ]]; then
+      timeout 25 bash oracle-2 $1 $TEST_ID
+  else
+      timeout 25 bash oracle-1 $1 $TEST_ID
+  fi
+
+fi
