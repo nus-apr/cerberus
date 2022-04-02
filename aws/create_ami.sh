29547530dd6a8ffaeca17a802bb45956eaab81a1
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-04-02 10:58:40 +0800

    update aws script

--- aws/create_ami.sh
+++ aws/create_ami.sh
@@ -4,3 +4,8 @@ sudo usermod -aG docker ubuntu
 sudo apt install -y pypy3 pypy3-dev
 sudo pypy3 -m easy_install docker more_itertools
 git clone https://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:x-oauth-basic@github.com/rshariffdeen/cerberus
+git clone https://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:x-oauth-basic@github.com/rshariffdeen/valkyrie
+sudo apt install python3.9 python3.9-distutils
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3.9 get-pip.py && rm get-pip.py
+python3.9 -m pip install pipenv virtualenv docker
+git submodule update --init --recursive
