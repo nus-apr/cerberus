66fad102d8b24cbe0c82d7329e1d3df9398ef6bd
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-04-19 02:39:13 +0800

    parameterize build/config and add darjeeling instrumentation

--- benchmark/manybugs/gzip/884ef6d16c/build.sh
+++ benchmark/manybugs/gzip/884ef6d16c/build.sh
@@ -7,8 +7,17 @@ dir_name=$1/$benchmark_name/$project_name/$bug_id
 cd $dir_name/src
 
 # Compile gzip
-make -e  CFLAGS="-g -O0" -j`nproc`
+PROJECT_CFLAGS="-g -O0"
 
+if [[ -n "${CFLAGS}" ]]; then
+  PROJECT_CFLAGS="${PROJECT_CFLAGS} ${CFLAGS}"
+fi
+
+if [[ -n "${LDFLAGS}" ]]; then
+  PROJECT_LDFLAGS="${PROJECT_LDFLAGS} ${LDFLAGS}"
+fi
+
+make -e CFLAGS="${PROJECT_CFLAGS}" LDFLAGS="${PROJECT_LDFLAGS}" -j`nproc`
 cd tests
 make mixed.log znew-k.log
 chmod +x mixed
