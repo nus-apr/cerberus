a1a97b375babc1a6794664695c7fa0c7c818d972
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-04-19 11:57:20 +0800

    parameterize config/build scripts and add darjeeling instrumentation

--- benchmark/manybugs/gmp/14167/build.sh
+++ benchmark/manybugs/gmp/14167/build.sh
@@ -9,7 +9,19 @@ cd $dir_name/src
 
 sed -i 's/no-dependencies ansi2knr/no-dependencies/g' Makefile;
 # Compile gmp
+
+
+PROJECT_CFLAGS="-g -O0 -static"
+
+if [[ -n "${CFLAGS}" ]]; then
+  PROJECT_CFLAGS="${PROJECT_CFLAGS} ${CFLAGS}"
+fi
+
+if [[ -n "${LDFLAGS}" ]]; then
+  PROJECT_LDFLAGS="${PROJECT_LDFLAGS} ${LDFLAGS}"
+fi
+
 make -e fib_table.h;make -e mp_bases.h;
-make -e  CFLAGS="-g -O0 -static" -j`nproc`
+make -e CFLAGS="${PROJECT_CFLAGS}" LDFLAGS="${PROJECT_LDFLAGS}" -j`nproc`
 cd tests/mpz
-make -e CFLAGS="-g -O0 -static" t-gcd
+make -e CFLAGS="${PROJECT_CFLAGS}" LDFLAGS="${PROJECT_LDFLAGS}"  t-gcd
--- benchmark/manybugs/gmp/14167/config.sh
+++ benchmark/manybugs/gmp/14167/config.sh
@@ -8,12 +8,31 @@ cd $dir_name/src
 
 # Config gmp.
 grep -v '"tests/mpbsd/Makefile") ' configure |   sed "s#tests/mpbsd/Makefile ##g" |   sponge configure
-make distclean
-CC=gcc CXX=g++ ./configure CFLAGS="-g -O0 -static"
-autoreconf --force --install
-CC=gcc CXX=g++ ./configure CFLAGS="-g -O0 -static" --disable-shared --enable-static \
+
+PROJECT_CFLAGS="-g -O0  -static"
+
+PROJECT_LDFLAGS=""
+PROJECT_CONFIG_OPTIONS=" --disable-shared --enable-static \
             --disable-fft \
             --disable-mpbsd \
             --disable-cxx \
             --disable-fast-install \
-            --disable-minithres
+            --disable-minithres "
+
+if [[ -n "${CFLAGS}" ]]; then
+  PROJECT_CFLAGS="${PROJECT_CFLAGS} ${CFLAGS}"
+fi
+if [[ -n "${CPPFLAGS}" ]]; then
+  PROJECT_CPPFLAGS="${PROJECT_CPPFLAGS} ${CPPFLAGS}"
+fi
+if [[ -n "${LDFLAGS}" ]]; then
+  PROJECT_LDFLAGS="${PROJECT_LDFLAGS} ${LDFLAGS}"
+fi
+if [[ -n "${CONFIG_OPTIONS}" ]]; then
+  PROJECT_CONFIG_OPTIONS="${PROJECT_CONFIG_OPTIONS} ${CONFIG_OPTIONS}"
+fi
+
+make distclean
+CC=gcc CXX=g++ ./configure CFLAGS="${PROJECT_CFLAGS}"
+autoreconf --force --install
+CC=gcc CXX=g++ ./configure CFLAGS="${PROJECT_CFLAGS}"  CXXFLAGS="${PROJECT_CXXFLAGS}" LDFLAGS="${PROJECT_LDFLAGS}" ${PROJECT_CONFIG_OPTIONS}
