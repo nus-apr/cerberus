f386fc11c1c9046781a3fe2949421383e2cc0604
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-04-20 19:48:34 +0800

    update prophet instrumentation

--- benchmark/manybugs/php/1e91069eb4/prophet/instrument.sh
+++ benchmark/manybugs/php/1e91069eb4/prophet/instrument.sh
@@ -35,6 +35,6 @@ Regression Cases: Tot 0
 EOF
 
 cd $dir_name
-prophet prophet/prophet.conf  -r workdir -init-only -o patches
+prophet prophet/prophet.conf  -r workdir -init-only -o patches -skip-verify
 cp $script_dir/profile_localization.res $dir_name/workdir
 
