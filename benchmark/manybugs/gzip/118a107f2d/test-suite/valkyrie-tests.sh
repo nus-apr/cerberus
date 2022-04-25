fa095da2dd95b365f4f630db07c00112bbba6923
rshariffdeen <rshariffdeen@gmail.com>
2022-04-25 20:52:40 +0800

    add valkyrie test-suite

---
+++ benchmark/manybugs/gzip/118a107f2d/test-suite/valkyrie-tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+TEST_ID=$1
+
+cd tests
+
+case "$TEST_ID" in
+    1) bash helin-segv;;
+    2) bash hufts;;
+    3) bash memcpy-abuse;;
+    4) bash mixed;;
+    5) bash null-suffix-clobber;;
+    6) bash stdin;;
+    7) bash trailing-nul;;
+    8) bash zdiff;;
+    9) bash zgrep-f;;
+    10) bash zgrep-signal;;
+    11) bash znew-k;;
+esac
+
+ret=$?
+exit $ret;
+
+#if [[ ret -eq 1 ]]
+#then
+#   exit 0;
+#else
+#
+#fi;
---
+++ benchmark/manybugs/gzip/118a107f2d/transform.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=$1/$benchmark_name/$project_name/$bug_id
+
+
+#copy shell-scripts
+cp $dir_name/src/zdiff test-suite
+cp $dir_name/src/znew test-suite
+cp $dir_name/src/zless test-suite
+cp $dir_name/src/zmore test-suite
+cp $dir_name/src/zfgrep test-suite
+cp $dir_name/src/zgrep test-suite
+cp $dir_name/src/zcat test-suite
+cp $dir_name/src/zcmp test-suite
+
+
+# copy binary executables
+#find -type f -executable -exec file -i '{}' \; | grep 'charset=binary' | grep -v "shellscript" | awk '{print $1}'
+cp -f $dir_name/src/gzip test-suite/src/gzip.orig
