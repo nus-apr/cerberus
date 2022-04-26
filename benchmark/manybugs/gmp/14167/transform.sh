1d97ea6623ab4b9530f72f078d372289cbcc435e
rshariffdeen <rshariffdeen@gmail.com>
2022-04-26 15:29:20 +0800

    update test-suite transformation to gmp

--- benchmark/manybugs/gmp/14167/transform.sh
+++ benchmark/manybugs/gmp/14167/transform.sh
@@ -22,14 +22,4 @@ do
 done
 
 
-# copy binary executables
-#find -type f -executable -exec file -i '{}' \; | grep 'charset=binary' | grep -v "shellscript" | awk '{print $1}'
-cp -f $dir_name/src/gzip test-suite/gzip.orig
-
-# copy test cases
-cp -rf $dir_name/src/tests test-suite
-
-# update path for test case
-sed -i 's#/experiment//manybugs/gzip/$bug_id/src/tests#/tmp#g' test-suite/tests/hufts
-
 
