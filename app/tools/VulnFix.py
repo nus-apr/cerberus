8f2c5bd975fd59ae2997032a15a1a95b250662f3
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-07-21 21:48:33 +0800

    remove initializing commands with updated tool image

--- app/tools/VulnFix.py
+++ app/tools/VulnFix.py
@@ -11,10 +11,6 @@ class VulnFix(AbstractTool):
         super(VulnFix, self).__init__(self.name)
         self.dir_root = "/home/yuntong/vulnfix"
         self.image_name = "yuntongzhang/vulnfix:latest"
-        pull_cmd = "git pull origin"
-        self.run_command(pull_cmd, dir_path=self.dir_root)
-        activate_cmd = "source activate"
-        self.run_command(activate_cmd, dir_path=self.dir_root)
 
 
     def repair(self, bug_info, config_info):
