1c477c299759be12c73ec0de7889769e74effeac
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-07-26 19:34:27 +0800

    update crepair instrumentation for potrace

--- benchmark/vulnloc/coreutils/gnubug-25003/crepair/instrument.sh
+++ benchmark/vulnloc/coreutils/gnubug-25003/crepair/instrument.sh
@@ -11,7 +11,7 @@ dir_exp:$dir_name
 tag_id:$bug_id
 src_directory:$dir_name/src
 binary_path:$dir_name/src/src/split
-config_command:CC=crepair-cc ./configure CFLAGS="-g -O0 -static -fPIE -Wno-error" CXXFLAGS="-g -O0 -static -fPIE -Wno-error"
+config_command:FORCE_UNSAFE_CONFIGURE=1 CC=crepair-cc ./configure CFLAGS="-g -O0 -static -fPIE -Wno-error" CXXFLAGS="-g -O0 -static -fPIE -Wno-error"
 build_command:make CC=crepair-cc CXX=crepair-cxx CFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" CXXFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" LDFLAGS="-static"
 test_input_list:-n7/75 /dev/null
 klee_flags:--link-llvm-lib=/CrashRepair/lib/libcrepair_proxy.bca
--- benchmark/vulnloc/coreutils/gnubug-26545/crepair/instrument.sh
+++ benchmark/vulnloc/coreutils/gnubug-26545/crepair/instrument.sh
@@ -11,7 +11,7 @@ dir_exp:$dir_name
 tag_id:$bug_id
 src_directory:$dir_name/src
 binary_path:$dir_name/src/src/shred
-config_command:CC=crepair-cc ./configure CFLAGS="-g -O0 -static -fPIE -Wno-error" CXXFLAGS="-g -O0 -static -fPIE -Wno-error"
+config_command:FORCE_UNSAFE_CONFIGURE=1 CC=crepair-cc ./configure CFLAGS="-g -O0 -static -fPIE -Wno-error" CXXFLAGS="-g -O0 -static -fPIE -Wno-error"
 build_command:make CC=crepair-cc CXX=crepair-cxx CFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" CXXFLAGS="-ggdb -fPIC -fPIE -g -O0 -Wno-error" LDFLAGS="-static"
 test_input_list:-n4 -s7 \$POC
 poc_list:$script_dir/../tests/1.txt
