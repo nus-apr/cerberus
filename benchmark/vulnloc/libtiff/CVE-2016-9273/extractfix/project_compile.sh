8f18aeecd9523e722113e7b7b90f049805e047f8
Martin Mirchev <mirchevmartin2203@gmail.com>
2022-08-05 11:34:22 +0800

    Add libtiff CVE-2016-9273 to ExtractFix benchmark

---
+++ benchmark/vulnloc/libtiff/CVE-2016-9273/extractfix/project_compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source=$1
+target=$2
+
+current_dir=`pwd`
+cd project
+KLEE_CFLAGS="-L${current_dir}/project_specific_lib/"
+PROJECT_CFLAGS="-llzma -lz -lm -ljpeg -ljbig -lhook"
+clang ${source} $PROJECT_CFLAGS $KLEE_CFLAGS -o ${target}
+cd ..
