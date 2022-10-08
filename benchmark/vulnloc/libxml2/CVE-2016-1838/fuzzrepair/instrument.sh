0f6f49cdf5df83ed1cdb60049dc9371af918adc8
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-10-08 10:19:30 +0800

    add fuzzrepair instrumentation

---
+++ benchmark/vulnloc/libxml2/CVE-2016-1838/fuzzrepair/instrument.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 4 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+bug_id=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+dir_name=$1/$benchmark_name/$project_name/$bug_id
+
+$script_dir/../config.sh $1
+$script_dir/../build.sh $1
+
+
