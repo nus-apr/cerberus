7c3653922756d43a8ea5fbc8ff8c62359a8ac802
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-10-20 19:25:26 +0800

    update fuzzrepair instrumentation

--- benchmark/vulnloc/libtiff/bugzilla-2611/fuzzrepair/instrument.sh
+++ benchmark/vulnloc/libtiff/bugzilla-2611/fuzzrepair/instrument.sh
@@ -8,7 +8,8 @@ dir_name=$1/$benchmark_name/$project_name/$bug_id
 fix_file=$dir_name/src/$2
 
 $script_dir/../config.sh $1
-cd $dir_name
+cd $dir_name/src
+make clean
 bear $script_dir/../build.sh $1
 cd $LIBPATCH_DIR/rewriter
 ./rewritecond $fix_file -o $fix_file
