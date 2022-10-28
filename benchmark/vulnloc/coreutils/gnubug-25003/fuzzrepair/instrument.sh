2d92442d5c1eae8719c9ba09e793af317c8a8e0f
Zhang Yuntong <ang.unong@gmail.com>
2022-10-28 13:03:50 +0800

    Use manual instrumentation for 25003

--- benchmark/vulnloc/coreutils/gnubug-25003/fuzzrepair/instrument.sh
+++ benchmark/vulnloc/coreutils/gnubug-25003/fuzzrepair/instrument.sh
@@ -7,14 +7,15 @@ dir_name=$1/$benchmark_name/$project_name/$bug_id
 
 fix_file=$dir_name/src/$2
 
+# first copy over a pre-instrumented version of fix file
+cp $script_dir/split.c $fix_file
+
 cd $dir_name/src
 
 # for AFL argv fuzz
-sed -i "1283i #include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"" src/split.c
+sed -i "1435i #include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"" src/split.c
 # dummy is just an empty file
-sed -i "1288i AFL_INIT_SET02(\"./split\", \"${dir_name}/src/dummy\");" src/split.c
-# avoid writing out a lot of files during fuzzing
-sed -i '595i return false;' src/split.c
+sed -i "1440i AFL_INIT_SET02(\"./split\", \"${dir_name}/src/dummy\");" src/split.c
 # not bulding man pages
 sed -i '229d' Makefile.am
 
@@ -24,19 +25,5 @@ cd $dir_name/src
 touch dummy
 # do make 
 make clean
-bear $script_dir/../build.sh $1
-# comment out this for rewriter to work
-sed -i "s|#include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"|//#include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"|g" src/split.c
-
-cd $LIBPATCH_DIR/rewriter
-./rewritecond $fix_file -o $fix_file
-ret=$?
-if [[ ret -eq 1 ]]
-then
-   exit 128
-fi
-# change back
-cd $dir_name/src
-sed -i "s|//#include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"|#include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"|g" src/split.c
 
 $script_dir/../build.sh $1
