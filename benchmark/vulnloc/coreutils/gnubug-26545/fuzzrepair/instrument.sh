4e3d907cca81288f3e303cc1178c8c824f01000a
Zhang Yuntong <ang.unong@gmail.com>
2022-10-28 16:05:03 +0800

    Add manual instrumentation for 26545

--- benchmark/vulnloc/coreutils/gnubug-26545/fuzzrepair/instrument.sh
+++ benchmark/vulnloc/coreutils/gnubug-26545/fuzzrepair/instrument.sh
@@ -7,17 +7,20 @@ dir_name=$1/$benchmark_name/$project_name/$bug_id
 
 fix_file=$dir_name/src/$2
 
+# first copy over a pre-instrumented version of fix file
+cp $script_dir/shred.c $fix_file
+
 cd $dir_name/src
 
-# for AFL argv fuzz
-sed -i "1215i #include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"" src/shred.c
-sed -i "1220i AFL_INIT_SET03(\"./shred\", \"${dir_name}/src/dummy\");" src/shred.c
-# -u option can cause a lot of files to be writting to disk during fuzzing; disable that
-sed -i '1260i break;' src/shred.c
-# remove and recreate output so that it does not grow too big.
-sed -i '1320i FILE* file_ptr = fopen(file[i], "w"); fclose(file_ptr);' src/shred.c
 # not bulding man pages
 sed -i '217d' Makefile.am
+# remove and recreate output so that it does not grow too big.
+sed -i '1370i FILE* file_ptr = fopen(file[i], "w"); fclose(file_ptr);' src/shred.c
+# -u option can cause a lot of files to be writting to disk during fuzzing; disable that
+sed -i '1309i break;' src/shred.c
+# for AFL argv fuzz
+sed -i "1268i AFL_INIT_SET03(\"./shred\", \"${dir_name}/src/dummy\");" src/shred.c
+sed -i "1264i #include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"" src/shred.c
 
 
 $script_dir/../config.sh $1
@@ -26,19 +29,4 @@ cd $dir_name/src
 touch dummy
 # do make
 make clean
-bear $script_dir/../build.sh $1
-# comment out this for rewriter to work
-sed -i "s|#include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"|//#include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"|g" src/shred.c
-
-cd $LIBPATCH_DIR/rewriter
-./rewritecond $fix_file -o $fix_file
-ret=$?
-if [[ ret -eq 1 ]]
-then
-   exit 128
-fi
-# change back
-cd $dir_name/src
-sed -i "s|//#include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"|#include \"${LIBPATCH_DIR}/helpers/argv-fuzz-inl.h\"|g" src/shred.c
-
 $script_dir/../build.sh $1
