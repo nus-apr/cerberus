01c099aa4aa119cc9b6e59a7417b812eadb8c70e
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-01-10 18:11:39 +0800

    remove manybugs dir and add as submodule

--- benchmark/manybugs/libtiff/207c78a/cpr/test-expected-output/short_test.smt2
+++
@@ -1,2 +0,0 @@
-(declare-const obs!0 (_ BitVec 32))
-(assert (= false (= obs!0 (_ bv0 32))))
--- benchmark/manybugs/php/1056c57fa9/test-suite/valkyrie-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-TEST_ID=$1
-./tester.py run $TEST_ID
-ret=$?
-exit $ret;
-
