a8200947476766679415638699d19d405a0e5591
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-03-15 18:15:18 +0800

    add repair driver for SAVER

---
+++ app/drivers/tools/SAVER.py
@@ -0,0 +1,135 @@
+import os
+import re
+from os import listdir
+from os.path import isfile
+from os.path import join
+from datetime import datetime
+from app.core import definitions
+from app.core import emitter
+from app.core import values
+from app.core.utilities import error_exit
+from app.core.utilities import execute_command
+from app.drivers.tools.AbstractTool import AbstractTool
+
+
+class SAVER(AbstractTool):
+    relative_binary_path = None
+    bug_conversion_table = {
+        "Memory Leak": "MEMORY_LEAK",
+        "Use After Free": "USE_AFTER_FREE",
+        "Double Free": "DOUBLE_FREE"
+    }
+
+    def __init__(self):
+        self.name = os.path.basename(__file__)[:-3].lower()
+        super(SAVER, self).__init__(self.name)
+
+    def populate_config_file(self, bug_info):
+        config_path = join(self.dir_expr, self.name, "bug.json")
+        config_info = dict()
+
+        bug_type = bug_info[definitions.KEY_BUG_TYPE]
+        if bug_type not in self.bug_conversion_table:
+            error_exit(f"Unsupported bug type: {bug_type}")
+
+        bug_type_code = self.bug_conversion_table[bug_type]
+
+        if definitions.KEY_SOURCE not in bug_info:
+            error_exit(f"Missing memory source information in benchmark, required for {self.name}")
+        if definitions.KEY_SINK not in bug_info:
+            error_exit(f"Missing memory sink information in benchmark, required for {self.name}")
+
+        config_info["source"] = {"node": bug_info[definitions.KEY_SOURCE], "exp": None}
+        config_info["sink"] = {"node": bug_info[definitions.KEY_SINK], "exp": None}
+        config_info["err_type"] = bug_type_code
+        self.write_file(config_info, config_path)
+        return config_path
+
+    def repair(self, bug_info, config_info):
+        super(SAVER, self).repair(bug_info, config_info)
+        if values.only_instrument:
+            return
+        emitter.normal("\t\t\t running repair with " + self.name)
+        conf_id = config_info[definitions.KEY_ID]
+        bug_id = str(bug_info[definitions.KEY_BUG_ID])
+        timeout_h = str(config_info[definitions.KEY_CONFIG_TIMEOUT])
+        additional_tool_param = config_info[definitions.KEY_TOOL_PARAMS]
+        config_path = self.populate_config_file(bug_info)
+        self.log_output_path = join(
+            self.dir_logs,
+            "{}-{}-{}-output.log".format(conf_id, self.name.lower(), bug_id),
+        )
+
+        if values.use_container:
+            emitter.error(
+                "[Exception] unimplemented functionality: SAVER docker support not implemented"
+            )
+            error_exit("Unhandled Exception")
+
+        self.timestamp_log_start()
+        saver_command = "cd {};".format(binary_dir_path)
+        saver_command += "timeout -k 5m {0}h saver --error-report {1} ".format(
+            str(timeout_h),
+            config_path
+        )
+        saver_command += "{0} >> {1} 2>&1 ".format(
+            additional_tool_param, self.log_output_path
+        )
+        status = execute_command(saver_command)
+        if status != 0:
+            emitter.warning(
+                "\t\t\t[warning] {0} exited with an error code {1}".format(
+                    self.name, status
+                )
+            )
+        else:
+            emitter.success("\t\t\t[success] {0} ended successfully".format(self.name))
+        emitter.highlight("\t\t\tlog file: {0}".format(self.log_output_path))
+        self.timestamp_log_end()
+
+    def save_artifacts(self, dir_info):
+        emitter.normal("\t\t\t saving artifacts of " + self.name)
+        copy_command = "cp -rf {}/saver {}".format(self.dir_expr, self.dir_output)
+        self.run_command(copy_command)
+        abs_binary_path = join(self.dir_expr, "src", self.relative_binary_path)
+        patch_path = abs_binary_path + ".bc.patch"
+        copy_command = "cp -rf {} {}/patches".format(patch_path, self.dir_output)
+        self.run_command(copy_command)
+        super(SAVER, self).save_artifacts(dir_info)
+        return
+
+    def analyse_output(self, dir_info, bug_id, fail_list):
+        emitter.normal("\t\t\t analysing output of " + self.name)
+        dir_results = join(self.dir_expr, "result")
+        conf_id = str(values.current_profile_id)
+        self.log_analysis_path = join(
+            self.dir_logs,
+            "{}-{}-{}-analysis.log".format(conf_id, self.name.lower(), bug_id),
+        )
+
+        regex = re.compile("(.*-output.log$)")
+        for _, _, files in os.walk(dir_results):
+            for file in files:
+                if regex.match(file) and self.name in file:
+                    self.log_output_path = dir_results + "/" + file
+                    break
+
+        if not self.log_output_path or not self.is_file(self.log_output_path):
+            emitter.warning("\t\t\t[warning] no output log file found")
+            return self._space, self._time, self._error
+
+        emitter.highlight("\t\t\t Log File: " + self.log_output_path)
+        is_error = False
+
+        log_lines = self.read_file(self.log_output_path, encoding="iso-8859-1")
+        self._time.timestamp_start = log_lines[0].replace("\n", "")
+        self._time.timestamp_end = log_lines[-1].replace("\n", "")
+        for line in log_lines:
+            if "of the total solutions found" in line:
+                self._space.plausible = int(line.split(": ")[-1])
+            elif "ERROR:" in line:
+                self._error.is_error = True
+        if is_error:
+            emitter.error("\t\t\t\t[error] error detected in logs")
+
+        return self._space, self._time, self._error
