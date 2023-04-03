42981b0c7d09e0483748985a0daa6915002dfa7f
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-04-03 13:59:17 +0800

    update analysis tools

--- app/drivers/tools/analyze/c/SAVER.py
+++ app/drivers/tools/analyze/c/SAVER.py
@@ -2,35 +2,38 @@ import os
 import re
 from datetime import datetime
 from os.path import join
-
 from app.core import definitions
 from app.core import emitter
 from app.core import values
-from app.drivers.tools.analysis.AbstractAnalysisTool import AbstractAnalysisTool
+from app.core.utilities import error_exit
+from app.drivers.tools.analyze.AbstractAnalysisTool import AbstractAnalysisTool
 
 
-class Pulse(AbstractAnalysisTool):
+class SAVER(AbstractAnalysisTool):
     relative_binary_path = None
 
     def __init__(self):
         self.name = os.path.basename(__file__)[:-3].lower()
-        super(Pulse, self).__init__(self.name)
-        self.image_name = "yuntongzhang/infer:latest"
-
+        super(SAVER, self).__init__(self.name)
 
     def prepare(self, bug_info):
         tool_dir = join(self.dir_expr, self.name)
         if not self.is_dir(tool_dir):
             self.run_command(f"mkdir -p {tool_dir}", dir_path=self.dir_expr)
-        emitter.normal("\t\t\t preparing subject for analysis with " + self.name)
+        emitter.normal("\t\t\t preparing subject for repair with " + self.name)
         dir_src = join(self.dir_expr, "src")
         clean_command = "make clean"
         self.run_command(clean_command, dir_path=dir_src)
 
         time = datetime.now()
         bug_type = bug_info[definitions.KEY_BUG_TYPE]
-        compile_command = (
-                "infer -j 20 capture -- make -j20"
+        if bug_type == "Memory Leak":
+            compile_command = (
+                "infer -j 20 -g --headers --check-nullable-only -- make -j20"
+            )
+        else:
+            compile_command = (
+                "infer -j 20 run -g --headers --check-nullable-only -- make -j20"
             )
         emitter.normal("\t\t\t\t compiling subject with " + self.name)
         self.run_command(compile_command, dir_path=dir_src)
@@ -43,18 +46,26 @@ class Pulse(AbstractAnalysisTool):
 
     def run_analysis(self, bug_info, config_info):
         self.prepare(bug_info)
-        super(Pulse, self).run_analysis(bug_info, config_info)
+        super(SAVER, self).run_analysis(bug_info, config_info)
+        if values.only_instrument:
+            return
         timeout_h = str(config_info[definitions.KEY_CONFIG_TIMEOUT])
         additional_tool_param = config_info[definitions.KEY_TOOL_PARAMS]
 
+        if values.use_container:
+            emitter.error(
+                "[Exception] unimplemented functionality: SAVER docker support not implemented"
+            )
+            error_exit("Unhandled Exception")
+
         self.timestamp_log_start()
-        analysis_command = "timeout -k 5m {0}h infer" \
-                           "--scheduler callgraph {1} ".format(
-            str(timeout_h), additional_tool_param
-        )
         bug_type = bug_info[definitions.KEY_BUG_TYPE]
         dir_src = join(self.dir_expr, "src")
-        status = self.run_command(analysis_command, dir_path=dir_src,
+        saver_command = "timeout -k 5m {0}h infer saver --pre-analysis-only {1}".format(
+            str(timeout_h), additional_tool_param
+        )
+
+        status = self.run_command(saver_command, dir_path=dir_src,
                                   log_file_path=self.log_output_path)
         if status != 0:
             emitter.warning(
@@ -64,16 +75,17 @@ class Pulse(AbstractAnalysisTool):
             )
         else:
             emitter.success("\t\t\t[success] {0} ended successfully".format(self.name))
-
         emitter.highlight("\t\t\tlog file: {0}".format(self.log_output_path))
         self.timestamp_log_end()
 
     def save_artifacts(self, dir_info):
         emitter.normal("\t\t\t saving artifacts of " + self.name)
+        copy_command = "cp -rf {}/saver {}".format(self.dir_expr, self.dir_output)
+        self.run_command(copy_command)
         infer_output = join(self.dir_expr, "src", "infer-out")
         copy_command = "cp -rf {} {}".format(infer_output, self.dir_output)
         self.run_command(copy_command)
-        super(Pulse, self).save_artifacts(dir_info)
+        super(SAVER, self).save_artifacts(dir_info)
         return
 
     def analyse_output(self, dir_info, bug_id, fail_list):
@@ -84,7 +96,21 @@ class Pulse(AbstractAnalysisTool):
             self.dir_logs,
             "{}-{}-{}-stats.log".format(conf_id, self.name.lower(), bug_id),
         )
+
+        regex = re.compile("(.*-output.log$)")
+        for _, _, files in os.walk(dir_results):
+            for file in files:
+                if regex.match(file) and self.name in file:
+                    self.log_output_path = dir_results + "/" + file
+                    break
+
+        if not self.log_output_path or not self.is_file(self.log_output_path):
+            emitter.warning("\t\t\t[warning] no output log file found")
+            return self._space, self._time, self._error
+
+        emitter.highlight("\t\t\t Log File: " + self.log_output_path)
         is_error = False
+
         log_lines = self.read_file(self.log_output_path, encoding="iso-8859-1")
         self._time.timestamp_start = log_lines[0].replace("\n", "")
         self._time.timestamp_end = log_lines[-1].replace("\n", "")
--- app/drivers/tools/analyze/multi/Infer.py
+++ app/drivers/tools/analyze/multi/Infer.py
@@ -2,39 +2,41 @@ import os
 import re
 from datetime import datetime
 from os.path import join
+
 from app.core import definitions
 from app.core import emitter
 from app.core import values
 from app.core.utilities import error_exit
-from app.drivers.tools.analysis.AbstractAnalysisTool import AbstractAnalysisTool
+from app.core.utilities import execute_command
+from app.drivers.tools.analyze.AbstractAnalysisTool import AbstractAnalysisTool
 
 
-class SAVER(AbstractAnalysisTool):
+class Infer(AbstractAnalysisTool):
     relative_binary_path = None
+    bug_conversion_table = {
+        "Memory Leak": "MEMORY_LEAK",
+        "Use After Free": "USE_AFTER_FREE",
+        "Double Free": "DOUBLE_FREE",
+    }
 
     def __init__(self):
         self.name = os.path.basename(__file__)[:-3].lower()
-        super(SAVER, self).__init__(self.name)
+        super(Infer, self).__init__(self.name)
 
     def prepare(self, bug_info):
         tool_dir = join(self.dir_expr, self.name)
         if not self.is_dir(tool_dir):
             self.run_command(f"mkdir -p {tool_dir}", dir_path=self.dir_expr)
-        emitter.normal("\t\t\t preparing subject for repair with " + self.name)
+        emitter.normal("\t\t\t preparing subject for analysis with " + self.name)
         dir_src = join(self.dir_expr, "src")
         clean_command = "make clean"
         self.run_command(clean_command, dir_path=dir_src)
 
         time = datetime.now()
-        bug_type = bug_info[definitions.KEY_BUG_TYPE]
-        if bug_type == "Memory Leak":
-            compile_command = (
-                "infer -j 20 -g --headers --check-nullable-only -- make -j20"
-            )
-        else:
-            compile_command = (
-                "infer -j 20 run -g --headers --check-nullable-only -- make -j20"
-            )
+        compile_command = (
+            "infer -j 20 -g compile -- make -j20"
+        )
+
         emitter.normal("\t\t\t\t compiling subject with " + self.name)
         self.run_command(compile_command, dir_path=dir_src)
         emitter.normal(
@@ -44,24 +46,16 @@ class SAVER(AbstractAnalysisTool):
         )
 
 
+
     def run_analysis(self, bug_info, config_info):
         self.prepare(bug_info)
-        super(SAVER, self).run_analysis(bug_info, config_info)
-        if values.only_instrument:
-            return
+        super(Infer, self).run_analysis(bug_info, config_info)
         timeout_h = str(config_info[definitions.KEY_CONFIG_TIMEOUT])
         additional_tool_param = config_info[definitions.KEY_TOOL_PARAMS]
 
-        if values.use_container:
-            emitter.error(
-                "[Exception] unimplemented functionality: SAVER docker support not implemented"
-            )
-            error_exit("Unhandled Exception")
-
         self.timestamp_log_start()
-        bug_type = bug_info[definitions.KEY_BUG_TYPE]
         dir_src = join(self.dir_expr, "src")
-        saver_command = "timeout -k 5m {0}h infer saver --pre-analysis-only {1}".format(
+        saver_command = "timeout -k 5m {0}h infer analyze {1} -- make -j20 ".format(
             str(timeout_h), additional_tool_param
         )
 
@@ -80,12 +74,10 @@ class SAVER(AbstractAnalysisTool):
 
     def save_artifacts(self, dir_info):
         emitter.normal("\t\t\t saving artifacts of " + self.name)
-        copy_command = "cp -rf {}/saver {}".format(self.dir_expr, self.dir_output)
-        self.run_command(copy_command)
         infer_output = join(self.dir_expr, "src", "infer-out")
         copy_command = "cp -rf {} {}".format(infer_output, self.dir_output)
         self.run_command(copy_command)
-        super(SAVER, self).save_artifacts(dir_info)
+        super(Infer, self).save_artifacts(dir_info)
         return
 
     def analyse_output(self, dir_info, bug_id, fail_list):
@@ -96,21 +88,7 @@ class SAVER(AbstractAnalysisTool):
             self.dir_logs,
             "{}-{}-{}-stats.log".format(conf_id, self.name.lower(), bug_id),
         )
-
-        regex = re.compile("(.*-output.log$)")
-        for _, _, files in os.walk(dir_results):
-            for file in files:
-                if regex.match(file) and self.name in file:
-                    self.log_output_path = dir_results + "/" + file
-                    break
-
-        if not self.log_output_path or not self.is_file(self.log_output_path):
-            emitter.warning("\t\t\t[warning] no output log file found")
-            return self._space, self._time, self._error
-
-        emitter.highlight("\t\t\t Log File: " + self.log_output_path)
         is_error = False
-
         log_lines = self.read_file(self.log_output_path, encoding="iso-8859-1")
         self._time.timestamp_start = log_lines[0].replace("\n", "")
         self._time.timestamp_end = log_lines[-1].replace("\n", "")
