f49722d149fc3f40c1498facae1937df3f0732c4
Martin Mirchev <mmirchev@comp.nus.edu.sg>
2023-07-25 09:38:48 +0800

    Small refactoring

--- app/core/configs/ConfigDataFactory.py
+++ app/core/configs/ConfigDataFactory.py
@@ -156,9 +156,7 @@ class ConfigDataFactory:
                     ToolConfig(
                         name=tool_config_dict[ConfigFieldsEnum.NAME.value],
                         params=tool_config_dict[ConfigFieldsEnum.PARAMS.value],
-                        tag=tool_config_dict[ConfigFieldsEnum.TAG.value]
-                        if ConfigFieldsEnum.TAG.value in tool_config_dict
-                        else "",
+                        tag=tool_config_dict.get(ConfigFieldsEnum.TAG.value, ""),
                     )
                 )
 
