b157014f2f24f9b0593e359611ac56990739ea84
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-08-07 09:50:47 +0800

    add read/write capabilities for csv

--- app/core/writer.py
+++ app/core/writer.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+import csv
 import json
 import pickle
 from typing import Any
@@ -14,3 +15,10 @@ def write_as_json(data: Any, output_file_path: str):
 def write_as_pickle(data: Any, output_file_path: str):
     with open(output_file_path, "wb") as out_file:
         pickle.dump(data, out_file)
+
+
+def write_as_csv(data: Any, output_file_path: str):
+    with open(output_file_path, "w", encoding="UTF8") as f:
+        writer = csv.writer(f)
+        for output in data:
+            writer.writerow(output)
