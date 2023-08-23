31a7ee5c724460d0cef07488bd01f99cbdba4a5d
xmcp <xmcp@pku.edu.cn>
2023-08-23 13:54:53 +0800

    implement et driver

--- app/drivers/tools/repair/java/ET.py
+++ app/drivers/tools/repair/java/ET.py
@@ -1,6 +1,6 @@
 import os
-from os.path import basename
-from os.path import join
+import json
+from pathlib import Path
 
 from app.drivers.tools.repair.AbstractRepairTool import AbstractRepairTool
 
@@ -9,142 +9,63 @@ class ET(AbstractRepairTool):
     def __init__(self):
         self.name = os.path.basename(__file__)[:-3].lower()
         super().__init__(self.name)
-        self.tbar_root_dir = "/TBar"
-        self.image_name = "mirchevmp/tbar-cerberus:latest"
+        self.image_name = "et-dev"
 
     def run_repair(self, bug_info, repair_config_info):
         super(ET, self).run_repair(bug_info, repair_config_info)
+        self.timestamp_log_start()
+
+        print('!!! begin')
+
+        ret = self.run_command('bash -c "echo OKAY"', log_file_path='/root/workflow/log.txt')
+        self.process_status(ret)
+        self.timestamp_log_end()
+        return
+        #####
+
+
         """
             self.dir_logs - directory to store logs
             self.dir_setup - directory to access setup scripts
             self.dir_expr - directory for experiment
             self.dir_output - directory to store artifacts/output
         """
-        if self.is_instrument_only:
-            return
 
-        dir_tbar_exist = self.is_dir(self.tbar_root_dir)
-        if not dir_tbar_exist:
-            # self.emit_error(
-            #     "[Exception] TBar repo is not at the expected location. "
-            #     "Please double check whether we are in TBar container."
-            # )
-            self.error_exit("TBar repo is not at the expected location.")
-        timeout_h = str(repair_config_info[self.key_timeout])
-        additional_tool_param = repair_config_info[self.key_tool_params]
+        workflow_path = Path('/root/workflow')
+        repo_path = (Path(self.dir_expr) / 'src').resolve()
+        setup_path = Path(self.dir_setup).resolve()
+        #print(bug_info, repair_config_info, self.container_id)
+        assert bug_info['language'] == 'java'
 
-        if self.container_id:
-            # Ensure that the container has git setup
-            self.run_command(
-                "bash -c 'git config --global user.email cerberus@nus-apr.com && git config --global user.name CERBERUS'",
-                dir_path=join(self.dir_expr, "src"),
-            )
+        self.write_file(json.dumps({
+            'repo_path': str(repo_path),
+            'setup_script_path': str(setup_path),
+            'sp_src': bug_info['source_directory'],
+            'sp_test': bug_info['test_directory'],
+            'tp_src': bug_info['class_directory'],
+            'tp_test': bug_info['test_class_directory'],
+            'cp_compile': ':'.join([str(Path(self.dir_expr)/s) for s in bug_info['dependencies']]),
+            'cp_test': ':'.join([
+                str(repo_path/bug_info['class_directory']),
+                str(repo_path/bug_info['test_class_directory']),
+                *[str(Path(self.dir_expr)/s) for s in bug_info['dependencies']],
+            ]),
+            'lang_level': bug_info['java_version'],
+            'test_passed': bug_info['passing_test'],
+            'test_failed': bug_info['failing_test'],
+            'test_timeout': bug_info['test_timeout'],
+        }), str(workflow_path/'info.json'))
 
-        # Ensure that there is a repo set up for the experiment and clean of any non-staged data
-        self.run_command(
-            "bash -c 'git init && git add . && git commit -m \"TEMP COMMIT\"'",
-            dir_path=join(self.dir_expr, "src"),
-        )
+        ret = self.run_command('bash -c "/root/miniconda3/envs/py39/bin/python3 /root/workflow/main.py"', log_file_path='/root/workflow/log.txt')
 
-        # prepare the required parameters
-        parameters = self.create_parameters(bug_info)
+        print(*self.read_file('/root/workflow/log.txt'), sep='')
 
-        command = (
-            'mvn compile exec:java -Dexec.mainClass="edu.lu.uni.serval.tbar.main.Main"'
-        )
-        args = (
-            "FAILING_TESTS='{}' ".format(" ".join(bug_info[self.key_failing_tests]))
-            + "CLASS_DIRECTORY={} ".format(bug_info[self.key_dir_class])
-            + "TEST_CLASS_DIRECTORY={} ".format(bug_info[self.key_dir_class])
-            + "SOURCE_DIRECTORY={} ".format(bug_info[self.key_dir_source])
-            + "TEST_SOURCE_DIRECTORY={} ".format(bug_info[self.key_dir_tests])
-        )
-
-        # start running
-        self.timestamp_log_start()
-        tbar_command = (
-            "bash -c '{4} timeout -k 5m {0}h {1} -Dexec.args=\"{2} {3}\"'".format(
-                timeout_h, command, parameters, additional_tool_param, args
-            )
-        )
-
-        status = self.run_command(
-            tbar_command,
-            log_file_path=self.log_output_path,
-            dir_path=self.tbar_root_dir,
-        )
-
-        self.process_status(status)
-
+        self.process_status(ret)
         self.timestamp_log_end()
+
         self.emit_highlight("log file: {0}".format(self.log_output_path))
 
-    def create_parameters(self, experiment_info):
-        """
-        Formats of execution cmds:
-            * ./PerfectFLTBarRunner.sh <Bug_Data_Path> <Bug_ID> <defects4j_Home> <Generate_All_Possible_Patches_Bool>
-            * ./NormalFLTBarRunner.sh <Bug_Data_Path> <Bug_ID> <defects4j_Home>
-
-        Examples:
-            * ./PerfectFLTBarRunner.sh D4J/projects/ Chart_8 D4J/defects4j/ false
-            * ./NormalFLTBarRunner.sh D4J/projects/ Chart_8 D4J/defects4j/
-        """
-
-        defects4j_home = "/defects4j/"
-        bug_id_str = "{0}_{1}".format(
-            experiment_info[self.key_subject],
-            experiment_info[self.key_bug_id],
-        )
-
-        """
-            create a symbolic link in the container:
-                /experiment/{benchmark}/{bug_subject}/{bug_id}/{bug_subject}_{bug_id} ->
-                                                                    /experiment/{benchmark}/{bug_subject}/{bug_id}/src/
-        """
-        bug_data_path_real = join(self.dir_expr, "src", "")
-        bug_data_path_symlink = join(self.dir_expr, bug_id_str)
-        symlink_command = "ln -s {0} {1}".format(
-            bug_data_path_real, bug_data_path_symlink
-        )
-        self.run_command(symlink_command)
-        fl_data = join(
-            self.tbar_root_dir, "SuspiciousCodePositions", bug_id_str, "Ochiai.txt"
-        )
-
-        if not self.is_file(fl_data):
-            self.error_exit(
-                "There is no fault localization data. This is currently unsupported"
-            )
-            # emitter.debug("Making 'weak' fault Localization")
-            # self.run_command(
-            #     "mkdir -p {}".format(
-            #         join(self.tbar_root_dir, "SuspiciousCodePositions", bug_id_str)
-            #     )
-            # )
-            # f = self.read_file(
-            #     join(
-            #         self.dir_expr,
-            #         "src",
-            #         experiment_info[self.key_dir_source],
-            #         experiment_info["source_file"].replace(".", "/") + ".java",
-            #     )
-            # )
-            # faulty_lines = [
-            #     "{}@{}\n".format(experiment_info["source_file"], l)
-            #     for l in range(len(f))
-            # ]
-
-            # self.write_file(faulty_lines, fl_data)
-
-        return " ".join(
-            [
-                self.dir_expr,
-                bug_id_str,
-                defects4j_home,
-                join(self.tbar_root_dir, "SuspiciousCodePositions"),
-                self.dir_output + "/",
-            ]
-        )
+        print('!!! end')
 
     def save_artifacts(self, dir_info):
         """
@@ -154,12 +75,12 @@ class ET(AbstractRepairTool):
         The parent method should be invoked at last to archive the results
         """
 
-        tbar_logs_dir = join(self.tbar_root_dir, "logs")
-        self.run_command("cp -r {0} {1}".format(tbar_logs_dir, self.dir_logs))
-
         # tbar_patches_dir = join(self.tbar_root_dir, "OUTPUT")
         # self.run_command("cp -r {0} {1}".format(tbar_patches_dir, self.dir_output))
 
+        self.run_command(f'cp -r /root/workflow/log.txt {self.dir_output}/')
+        self.run_command(f'cp -r /root/workflow/fl {self.dir_output}/')
+        self.run_command(f'cp -r /root/workflow/repair {self.dir_output}/')
         super().save_artifacts(dir_info)
         return
 
@@ -183,64 +104,12 @@ class ET(AbstractRepairTool):
         """
         self.emit_normal("reading output")
 
-        is_error = False
-        count_generated = 0
-        count_plausible = 0
-        count_enumerations = 0
-        count_non_compilable = 0
+        self.stats.patch_stats.generated = 0
+        self.stats.patch_stats.plausible = 0
+        self.stats.patch_stats.enumerations = 0
+        self.stats.patch_stats.size = 0
+        self.stats.patch_stats.non_compilable = 0
 
-        # count number of patch files
-        # available only for FixPatterns
-        list_patches_files_set = set()
-        dir_output_fix_patterns = join(
-            self.tbar_root_dir, "OUTPUT", "FixPatterns", "TBar"
-        )
-        list_output_fix_pattern_tbar_dir = self.list_dir(dir_output_fix_patterns)
-        for dir_name in list_output_fix_pattern_tbar_dir:
-            dir_fixed_bugs = join(dir_name, "FixedBugs")
-            dir_fixed_bugs_ids_str = self.list_dir(dir_fixed_bugs)
-            for dir_bug_id_str_name in dir_fixed_bugs_ids_str:
-                list_patches_files = self.list_dir(dir_bug_id_str_name)
-                for file_patch_path in list_patches_files:
-                    file_patch_name = basename(file_patch_path)
-                    if (
-                        "Patch" in file_patch_name
-                        and file_patch_name not in list_patches_files_set
-                    ):
-                        self.stats.patch_stats.generated += 1
-                        count_generated += 1
-        self.stats.patch_stats.generated = count_generated
-
-        # extract information from output log
-        if not self.log_output_path or not self.is_file(self.log_output_path):
-            self.emit_warning("no output log file found")
-            return self.stats
-
-        self.emit_highlight(f"output log file: {self.log_output_path}")
-
-        if self.is_file(self.log_output_path):
-            log_lines = self.read_file(self.log_output_path, encoding="iso-8859-1")
-            self.stats.time_stats.timestamp_start = log_lines[0].replace("\n", "")
-            self.stats.time_stats.timestamp_end = log_lines[-1].replace("\n", "")
-
-            for line in log_lines:
-                if "Patch Candidate" in line:
-                    count_enumerations += 1
-                if "failed compiling" in line:
-                    count_non_compilable += 1
-                if "Succeeded to fix the bug" in line:
-                    count_plausible += 1
-                    count_enumerations += 1
-                elif (
-                    "Partial succeeded to fix bug" in line
-                    or "Failed to fix bug" in line
-                ):
-                    count_non_compilable += 1
-                    count_enumerations += 1
-
-        self.stats.patch_stats.plausible = count_plausible
-        self.stats.patch_stats.enumerations = count_enumerations
-        self.stats.patch_stats.non_compilable = count_non_compilable
-        self.stats.error_stats.is_error = is_error
+        self.stats.error_stats.is_error = False
 
         return self.stats
