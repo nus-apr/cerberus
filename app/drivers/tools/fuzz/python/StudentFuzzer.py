e33621b0c30517edb07e0b822e17dc34a503aaa6
Dylan J. Wolff <wolffdy0@gmail.com>
2023-08-29 17:40:51 +0800

    updating fuzzer and benchmark (#121)

--- app/drivers/tools/fuzz/python/StudentFuzzer.py
+++ app/drivers/tools/fuzz/python/StudentFuzzer.py
@@ -2,6 +2,7 @@ import os
 from os.path import join
 
 from app.drivers.tools.fuzz.AbstractFuzzTool import AbstractFuzzTool
+from app.core import container
 
 
 class StudentFuzzer(AbstractFuzzTool):
@@ -16,22 +17,53 @@ class StudentFuzzer(AbstractFuzzTool):
         output of the tool is logged at self.log_output_path
         information required to be extracted are:
         """
+
+        r = self.list_dir("/home/student", regex = "*")
+        self.emit_normal(f"saw files {r}")
+
+        if f"/home/student/{self.nonce}.crash" in r:
+            command_str =  f"date -r {self.nonce}.crash +%s"
+            exit_code, output = container.exec_command(
+                self.container_id, command_str, "/home/student", dict()
+            )
+            stdout, stderr = output
+            self.stats.fuzzing_stats.time_to_bug = (int(stdout) - self.stime)
+        else:
+            self.stats.fuzzing_stats.time_to_bug = -1
+
         return self.stats
 
     def run_fuzz(self, bug_info, fuzz_config_info):
         super(StudentFuzzer, self).run_fuzz(bug_info, fuzz_config_info)
         self.emit_normal("executing fuzz command")
+        self.nonce = "12345"
 
         timeout_mins = int(float(fuzz_config_info[self.key_timeout]) * 60)
 
         self.run_command(
-            "bash -c 'cp -rf {} /home/student/'".format(join(self.dir_expr, "src", "*"))
+            "bash -c 'cp -Rf {} /home/student/'".format(join(self.dir_expr, "."))
         )
 
+        command_str =  f"date +%s"
+        exit_code, output = container.exec_command(
+            self.container_id, command_str, "/home/student", dict()
+        )
+        stdout, stderr = output
+        self.stime = int(stdout)
+
         self.timestamp_log_start()
+
+
+        # @TODO replace when real timeouts are sorted out
+        # repair_command = (
+        #     "bash -c 'NONCE={} timeout -k 20s {}m python3 student_fuzzer.py'".format(
+        #         self.nonce,
+        #         timeout_mins
+        #     )
+        # )
         repair_command = (
-            "bash -c 'NONCE=12345 timeout -k 5m {}m python3 student_fuzzer.py'".format(
-                timeout_mins
+            "bash -c 'NONCE={} timeout -k 2m 2m python3 student_fuzzer.py'".format(
+                self.nonce,
             )
         )
 
