2aa82e06140d053cd7656fe22eb368956d6f0293
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-08-30 15:07:09 +0800

    update instrumented driver, call grandparent init instead of parent init

--- app/drivers/benchmarks/c/VulnLocInstrumented.py
+++ app/drivers/benchmarks/c/VulnLocInstrumented.py
@@ -6,4 +6,4 @@ from app.drivers.benchmarks.c.VulnLoc import VulnLoc
 class VulnLocInstrumented(VulnLoc):
     def __init__(self):
         self.name = os.path.basename(__file__)[:-3].lower()
-        super().__init__()
+        super(VulnLoc, self).__init__()
