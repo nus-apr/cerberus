0ce69299bd3e28db1a814f46dc13627684fd8f57
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-09-08 21:17:18 +0800

    override verify for instrumented vulnloc

--- app/drivers/benchmarks/c/VulnLocInstrumented.py
+++ app/drivers/benchmarks/c/VulnLocInstrumented.py
@@ -7,3 +7,8 @@ class VulnLocInstrumented(VulnLoc):
     def __init__(self):
         self.name = os.path.basename(__file__)[:-3].lower()
         super(VulnLoc, self).__init__()
+
+    def verify(self, bug_index, container_id):
+        self.emit_normal("verify dev patch and test-oracle")
+        self.emit_debug("verify took 0 second(s)")
+        return 1
