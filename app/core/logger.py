896ee088cad332b483063dd7f9be4b958707ff3f
Martin Mirchev <mmirchev@comp.nus.edu.sg>
2023-09-23 15:33:17 +0800

    Add job tracking

--- app/core/logger.py
+++ app/core/logger.py
@@ -58,12 +58,19 @@ def store_logs():
         store_log_file(log_f)
 
 
+def track_job(txt):
+    job = values.job_identifier.get("NAN")
+    if job != "NAN":
+        return job + "    " + txt
+    return txt
+
+
 def build(message):
-    _logger_build.info(message)
+    _logger_build.info(track_job(message))
 
 
 def information(message):
-    _logger_main.info(message)
+    _logger_main.info(track_job(message))
 
 
 def trace(message, info):
@@ -73,50 +80,50 @@ def trace(message, info):
 def command(message):
     message = str(message).strip().replace("[command]", "")
     message = "[COMMAND]: {}\n".format(message)
-    _logger_main.info(message)
-    _logger_command.info(message)
+    _logger_main.info(track_job(message))
+    _logger_command.info(track_job(message))
 
 
 def docker_command(message):
     message = str(message).strip().replace("[command]", "")
     message = "[DOCKER-COMMAND]: {}\n".format(message)
-    _logger_main.info(message)
-    _logger_command.info(message)
+    _logger_main.info(track_job(message))
+    _logger_command.info(track_job(message))
 
 
 def data(message, info):
-    _logger_main.info(message, info)
+    _logger_main.info(track_job(message), info)
 
 
 def debug(message):
     message = str(message).strip()
-    _logger_main.debug(message)
+    _logger_main.debug(track_job(message))
 
 
 def error(message):
-    _logger_main.error(message)
-    _logger_error.error(message)
+    _logger_main.error(track_job(message))
+    _logger_error.error(track_job(message))
 
 
 def note(message):
-    _logger_main.info(message)
+    _logger_main.info(track_job(message))
 
 
 def configuration(message):
     message = str(message).strip().lower().replace("[config]", "")
     message = "[CONFIGURATION]: {}\n".format(message)
-    _logger_main.info(message)
+    _logger_main.info(track_job(message))
 
 
 def output(message):
     message = str(message).strip()
     message = "[OUTPUT]: {}".format(message)
-    _logger_main.info(message)
+    _logger_main.info(track_job(message))
 
 
 def warning(message):
     message = str(message).strip().lower().replace("[warning]", "")
-    _logger_main.warning(message)
+    _logger_main.warning(track_job(message))
 
 
 def log_tool_stats(task_tag_name, tool_stats: ToolStats):
