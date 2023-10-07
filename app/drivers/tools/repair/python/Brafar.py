217caf3196da2929d26e3235d421a0a8003661fe
RazorX7 <39217349+RazorX7@users.noreply.github.com>
2023-10-07 13:08:46 +0800

    [APRComp24] Brafar (#136)
    * update tool
    
    * update tool hash
    
    * update tool hash
    
    * update tool hash
    
    ---------
    
    Signed-off-by: Martin Mirchev <mmirchev@comp.nus.edu.sg>
    Co-authored-by: Martin Mirchev <mmirchev@comp.nus.edu.sg>

--- app/drivers/tools/repair/python/Brafar.py
+++ app/drivers/tools/repair/python/Brafar.py
@@ -1,6 +1,6 @@
 import os
 from os.path import join
-
+from app.core import container
 from app.drivers.tools.repair.AbstractRepairTool import AbstractRepairTool
 
 
@@ -9,16 +9,18 @@ class Brafar(AbstractRepairTool):
         self.name = os.path.basename(__file__)[:-3].lower()
         super().__init__(self.name)
         self.image_name = "linnaxie/brafar-python"
-        self.hash_digest = "sha256:bcd4a6029e242260e2efa50abc61e14338512586c0f10ede0ec2a466e9226c7a"
+        self.hash_digest = "sha256:e9a2c6ed1ca1ed635a465d94a93b0fda9d883156cfbc0c102f99655559e3a3d7"
+
 
     def run_repair(self, bug_info, repair_config_info):
         # print(self.dir_expr)
         super(Brafar, self).run_repair(bug_info, repair_config_info)
         self.timestamp_log_start()
         status = self.run_command(
-            "timeout -k 5m {}h python3 run.py -d {} -q src -s 100 {}".format(
+            "timeout -k 5m {}h python3 run.py -d {} -q src -s 100 -o {} {}".format(
                 repair_config_info[self.key_timeout],
                 self.dir_expr,
+                "/output/patches",
                 repair_config_info[self.key_tool_params],
             ),
             self.log_output_path,
@@ -35,9 +37,10 @@ class Brafar(AbstractRepairTool):
         logs folder -> self.dir_logs
         The parent method should be invoked at last to archive the results
         """
-        self.run_command("mkdir /output")
-        self.run_command("mkdir /output/patches")
-        self.run_command("bash -c 'cp {}src/*.diff /output/patches'".format(self.dir_expr))
+        # self.run_command("mkdir /output")
+        # self.run_command("mkdir /output/patches")
+        # self.run_command("bash -c 'cp {}/src/*.diff /output/patches'".format(self.dir_expr))
+
         super(Brafar, self).save_artifacts(dir_info)
 
     def analyse_output(self, dir_info, bug_id, fail_list):
@@ -45,20 +48,37 @@ class Brafar(AbstractRepairTool):
         analyse tool output and collect information
         output of the tool is logged at self.log_output_path
         information required to be extracted are:
-
-            self.stats.patches_stats.non_compilable
-            self.stats.patches_stats.plausible
-            self.stats.patches_stats.size
-            self.stats.patches_stats.enumerations
-            self.stats.patches_stats.generated
-
-            self.stats.time_stats.total_validation
-            self.stats.time_stats.total_build
-            self.stats.time_stats.timestamp_compilation
-            self.stats.time_stats.timestamp_validation
-            self.stats.time_stats.timestamp_plausible
         """
-        self.emit_normal("reading output")
+        # self.stats.patches_stats.non_compilable
+        # self.stats.patches_stats.plausible
+        # self.stats.patches_stats.size = 1
+        # self.stats.patches_stats.enumerations
+        # self.stats.patches_stats.generated
+        #
+        # self.stats.time_stats.total_validation
+        # self.stats.time_stats.total_build
+        # self.stats.time_stats.timestamp_compilation
+        # self.stats.time_stats.timestamp_validation
+        # self.stats.time_stats.timestamp_plausible
+        if not self.log_output_path or not self.is_file(self.log_output_path):
+            self.emit_warning("no output log file found")
+            return self.stats
 
-        # extract information from output log
+        self.emit_highlight(f"output log file: {self.log_output_path}")
+
+        if self.is_file(self.log_output_path):
+            log_lines = self.read_file(self.log_output_path, encoding="iso-8859-1")
+            self.stats.time_stats.timestamp_start = log_lines[0].replace("\n", "")
+            self.stats.time_stats.timestamp_end = log_lines[-1].replace("\n", "")
+            self.stats.patch_stats.enumerations = 1
+
+            for line in log_lines:
+                if line.startswith("fail"):
+                    self.stats.error_stats.is_error = True
+                if line.startswith("The repair result is:"):
+                    if "True" in line:
+                        self.stats.patch_stats.plausible = 1
+                    elif "False" in line:
+                        self.stats.patch_stats.implausible = 1
+
         return self.stats
