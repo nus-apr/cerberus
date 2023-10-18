45c676e58879e0962b371fbadd61d3c609bd02df
Sergey Mechtaev <mechtaev@gmail.com>
2023-10-18 20:52:21 +0800

    update f1x repo and digest (#134)

--- app/core/configuration.py
+++ app/core/configuration.py
@@ -103,7 +103,6 @@ class Configurations:
     __slack_config_file = open(join(values.dir_config, "slack.json"))
     __discord_config_file = open(join(values.dir_config, "discord.json"))
     __default_config_values: Dict[str, Any] = {
-        "patch-rank-limit": 5,
         "stack-size": 15000,
         "use-cache": False,
         "use-gpu": False,
