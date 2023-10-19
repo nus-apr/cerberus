b3051390cb724466ebd7d75ad99dd1d06b76c156
crhf <ruanhf8@gmail.com>
2023-10-19 14:03:41 +0800

    make RewardRepair work with multiple locations; fix RewardRepair timeout

--- app/drivers/tools/repair/java/RewardRepairI.py
+++ app/drivers/tools/repair/java/RewardRepairI.py
@@ -1,6 +1,9 @@
+import csv
 import os
+import time
 from os.path import join
 
+from app.core import definitions, values
 from app.drivers.tools.repair.AbstractRepairTool import AbstractRepairTool
 
 
@@ -9,11 +12,14 @@ class RewardRepairI(AbstractRepairTool):
     Requirements for this tool: 2GB of VRAM (for GPUs)
     """
 
+    SECS_PER_HOUR = 3600
+
     def __init__(self):
         self.name = os.path.basename(__file__)[:-3].lower()
         super().__init__(self.name)
         self.image_name = "thanhlecong/rewardrepair:cerberus"
         self.bug_name = ""
+        self.dir_fl = join(values.dir_main, "localization", "ochiai")
 
     def run_repair(self, bug_info, repair_config_info):
         super(RewardRepairI, self).run_repair(bug_info, repair_config_info)
@@ -24,49 +30,77 @@ class RewardRepairI(AbstractRepairTool):
             self.dir_output - directory to store artifacts/output
         """
 
-        timeout_h = str(repair_config_info[self.key_timeout])
+        top_n = 200
 
-        if len(bug_info[self.key_fix_lines]) == 0:
-            self.error_exit("no line number to fix")
+        tool_dir = "/repair/RewardRepair"
 
-        self.bug_name = bug_info[self.key_bug_id]
-        file = (
-            join(
-                bug_info[self.key_dir_source],
-                bug_info[self.key_fix_file].replace(".", "/"),
+        if repair_config_info[definitions.KEY_CONFIG_FIX_LOC] == "dev":
+            if len(bug_info[self.key_fix_lines]) == 0:
+                self.error_exit("no line number to fix")
+
+            locations = [
+                (
+                    bug_info[self.key_fix_file].replace(".", "/"),
+                    bug_info[self.key_fix_lines][0],
+                )
+            ]
+        else:
+            fl_file = join(self.dir_fl, f"{bug_info[self.key_bug_id]}.csv")
+            if not os.path.isfile(fl_file):
+                self.error_exit(f"Localization file not found: {fl_file}")
+
+            with open(fl_file, newline="") as f:
+                reader = csv.reader(f)
+                locations = [
+                    (the_class.replace(".", "/"), line)
+                    for (the_class, line, _) in reader
+                ]
+
+        self.timestamp_log_start()
+        time_start = time.time()
+
+        timeout_h = float(repair_config_info[self.key_timeout])
+
+        for fix_file, fix_line in locations:
+            out_dir = join(self.dir_output, f"{fix_file.replace('/', '.')}-{fix_line}")
+            self.run_command(f"mkdir -p {out_dir}", self.log_output_path, tool_dir)
+
+            self.bug_name = bug_info[self.key_bug_id]
+            file = join(bug_info[self.key_dir_source], f"{fix_file}.java")
+
+            elapsed_h = (time.time() - time_start) / self.SECS_PER_HOUR
+            if elapsed_h > timeout_h:
+                break
+
+            repair_command = (
+                f"""bash -c 'export PATH=$PATH:/root/defects4j/framework/bin &&"""
+                f""" timeout -k 5m {timeout_h - elapsed_h}h python3 main.py --task repair"""
+                f""" --bug_id {self.bug_name.replace("-", "_")}"""
+                f""" --src_dir {join(self.dir_expr, "src")}"""
+                f""" --buggy_file {join(file)} --buggy_loc {fix_line}"""
+                f""" --top_n_patches {top_n}"""
+                f""" --output_folder {out_dir}'"""
             )
-            + ".java"
-        )
+            status = self.run_command(repair_command, self.log_output_path, tool_dir)
+            self.process_status(status)
 
-        repair_command = "bash -c 'export PATH=$PATH:/root/defects4j/framework/bin && timeout -k 5m {}h python3 main.py --task repair --bug_id {} --src_dir {} --buggy_file {} --buggy_loc {} --top_n_patches {} --output_folder {}'".format(  # currently supporting only defects4j
-            timeout_h,
-            self.bug_name.replace("-", "_"),
-            join(self.dir_expr, "src"),
-            join(file),
-            bug_info[self.key_fix_lines][0],
-            200,  # top_n_patches
-            join(self.dir_output),
-        )
-        status = self.run_command(
-            repair_command, self.log_output_path, "/repair/RewardRepair"
-        )
+            elapsed_h = (time.time() - time_start) / self.SECS_PER_HOUR
+            if elapsed_h > timeout_h:
+                break
 
-        repair_command = "bash -c 'export PATH=$PATH:/root/defects4j/framework/bin && timeout -k 5m {}h python3 main.py --task validate --bug_id {} --src_dir {} --buggy_file {} --buggy_loc {} --output_folder {}'".format(
-            timeout_h,
-            self.bug_name.replace("-", "_"),
-            join(self.dir_expr, "src"),
-            join(file),
-            bug_info[self.key_fix_lines][0],
-            join(self.dir_output),
-        )
+            validation_command = (
+                f"""bash -c 'export PATH=$PATH:/root/defects4j/framework/bin &&"""
+                f""" timeout -k 5m {timeout_h - elapsed_h}h python3 main.py --task validate"""
+                f""" --bug_id {self.bug_name.replace("-", "_")}"""
+                f""" --src_dir {join(self.dir_expr, "src")}"""
+                f""" --buggy_file {join(file)} --buggy_loc {fix_line}"""
+                f""" --output_folder {out_dir}'"""
+            )
 
-        status = self.run_command(
-            repair_command,
-            self.log_output_path,
-            "/repair/RewardRepair",
-        )
-
-        self.process_status(status)
+            status = self.run_command(
+                validation_command, self.log_output_path, tool_dir
+            )
+            self.process_status(status)
 
         self.timestamp_log_end()
         self.emit_highlight("log file: {0}".format(self.log_output_path))
