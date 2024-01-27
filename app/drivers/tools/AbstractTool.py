0e45510120d7482071df2b373ae6f59b4d4ecbcb
Martin Mirchev <mmirchev@comp.nus.edu.sg>
2024-01-27 15:26:42 +0800

    Add clean script and command keywords

--- app/drivers/tools/AbstractTool.py
+++ app/drivers/tools/AbstractTool.py
@@ -60,10 +60,12 @@ class AbstractTool(AbstractDriver):
     key_sink = definitions.KEY_SINK
     key_compile_programs = definitions.KEY_COMPILE_PROGRAMS
     key_build_command = definitions.KEY_BUILD_COMMAND
+    key_clean_command = definitions.KEY_CLEAN_COMMAND
     key_config_command = definitions.KEY_CONFIG_COMMAND
     key_build_script = definitions.KEY_BUILD_SCRIPT
     key_config_script = definitions.KEY_CONFIG_SCRIPT
     key_test_script = definitions.KEY_TEST_SCRIPT
+    key_clean_script = definitions.KEY_CLEAN_SCRIPT
     key_exploit_inputs = definitions.KEY_EXPLOIT_INPUTS
     key_benign_inputs = definitions.KEY_BENIGN_INPUTS
     key_pub_test_script = definitions.KEY_PUB_TEST_SCRIPT
