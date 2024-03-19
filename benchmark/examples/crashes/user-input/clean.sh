ca87c936943fed01ba582ad1c2ff35663e668642
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2024-03-19 10:41:38 +0800

    add clean script

---
+++ benchmark/examples/crashes/user-input/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
+project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
+bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
+dir_name=/experiment/$benchmark_name/$project_name/$bug_id
+cd $dir_name/src
+
+make clean
