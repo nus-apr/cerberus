773798742df09878533fc6f45ed5fd490c6f6e5a
Nan Jiang <61022133+jiang719@users.noreply.github.com>
2024-03-21 04:15:05 -0400

    add aixcc prompt (#175)

--- app/drivers/tools/repair/c/AIxCCRepairPrompt.py
+++ app/drivers/tools/repair/c/AIxCCRepairPrompt.py
@@ -8,17 +8,19 @@ from app.drivers.tools.repair.AbstractRepairTool import AbstractRepairTool
 
 CUR_DIR = os.path.abspath(__file__)[: os.path.abspath(__file__).rindex("/") + 1]
 
-
-class RepairCATPython(AbstractRepairTool):
+"""
+Prompt GPT-4 to fix vulnerability
+"""
+class AIxCCRepairPrompt(AbstractRepairTool):
     def __init__(self):
         self.name = os.path.basename(__file__)[:-3].lower()
         super().__init__(self.name)
-        self.image_name = "jiang719/repaircat-autocode-python:second-pr"
+        self.image_name = "jiang719/aixcc-repair-prompt:latest"
         self.bug_name = ""
-        self.hash_digest = "sha256:48894e7d2897"
+        self.hash_digest = "sha256:3990b225f24b39ad229f0c3af19fd12f23f178141d564c21e32ba833cb96e483"
 
     def run_repair(self, bug_info, repair_config_info):
-        super(RepairCATPython, self).run_repair(bug_info, repair_config_info)
+        super(AIxCCRepairPrompt, self).run_repair(bug_info, repair_config_info)
         """
             self.dir_logs - directory to store logs
             self.dir_setup - directory to access setup scripts
@@ -32,23 +34,19 @@ class RepairCATPython(AbstractRepairTool):
             "dir_logs": self.dir_logs,
             "dir_setup": self.dir_setup,
             "dir_expr": self.dir_expr,
-            "dir_output": self.dir_output,
-            "key_test_script": self.key_test_script,
+            "dir_output": self.dir_output
         }
         bug_info_encoded = base64.b64encode(json.dumps(bug_info).encode()).decode()
         tool_info_encoded = base64.b64encode(json.dumps(tool_info).encode()).decode()
-        # expected name for test-oracle
-        self.run_command(
-            f"cp {self.dir_setup}/{bug_info[self.key_test_script]} {self.dir_setup}/run_test"
-        )
+
         timeout_h = str(repair_config_info[self.key_timeout])
-        repair_command = f"bash -c 'cd /home/repaircat-autocode-python/api && python repair.py {bug_info_encoded} {tool_info_encoded}'"
+        repair_command = f"bash -c 'cd /home/aixcc-repair/api && python prompt_repair.py {bug_info_encoded} {tool_info_encoded}'"
         repair_command = f"timeout -k 5m {timeout_h}h {repair_command} "
         self.timestamp_log_start()
         status = self.run_command(
             repair_command,
             self.log_output_path,
-            dir_path="/home/repaircat-autocode-python/",
+            dir_path="/home/aixcc-repair/",
         )
 
         self.run_command(f"cp /home/result.json {self.dir_output}/result.json")
