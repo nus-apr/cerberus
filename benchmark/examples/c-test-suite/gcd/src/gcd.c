338396e6dd409044846d104c02e06d3105273635
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2024-03-26 12:47:52 +0800

    add new examples and remove old examples; organize workflows

--- benchmark/examples/c-test-suite/gcd/src/gcd.c
+++ benchmark/examples/c-test-suite/gcd/src/gcd.c
@@ -7,20 +7,19 @@ int main(int argc, char *argv[]) {
   a = atoi(argv[1]);
   b = atoi(argv[2]);
 
-  /* jump:12 */ if (a == 0) {
+  if (a == 0) {
     printf("%g\n", b);
   }
-  {
-   /* jump:21 */ while (b != 0) {
-      /* jump:17 */  if (a > b) {
+
+    while (b != 0) {
+      if (a > b) {
         a = a - b;
       } else {
         b = b - a;
       }
-      fprintf(fopen("/dev/null", "w"), "valkyrie\n");
     }
     printf("%g\n", a);
-  }
+
 
   return 0;
 }
---
+++ benchmark/examples/python-test-suite/calculator/src/test_3.py
@@ -0,0 +1,5 @@
+import calculator
+
+
+def test_3() -> None:
+    assert calculator.Calculator().add_five(x=-5) == 0
