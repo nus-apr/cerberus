b85f60beda7bceb92b23f2c8e9d75ce560b63ced
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2024-04-03 12:35:29 +0800

    update driver with dynamic instrumentation

--- app/drivers/tools/localize/c/CrashRepair.py
+++ app/drivers/tools/localize/c/CrashRepair.py
@@ -20,7 +20,7 @@ class CrashRepair(AbstractLocalizeTool):
 
     def instrument(self, bug_info: Dict[str, Any]) -> None:
         """instrumentation for the experiment as needed by the tool"""
-        self.emit_normal("Running instrumentation")
+        self.emit_normal("Running dynamic instrumentation")
         bug_id = bug_info[self.key_bug_id]
         dir_src = join(self.dir_expr, "src")
         task_conf_id = str(self.current_task_profile_id.get("NA"))
@@ -29,20 +29,25 @@ class CrashRepair(AbstractLocalizeTool):
             "{}-{}-{}-instrument.log".format(task_conf_id, self.name, bug_id),
         )
         inst_script_path = self.dir_expr + f"/{self.name}-instrument"
-        self.write_file(
-            [
-                'grep -nrl -e "fabs" | grep "\.c" | xargs -I @ sed -i \'s/ fabs/ fabs_crepair/g\' @\n',
-                'grep -nrl -e "longjmp" | grep "\.c" | xargs -I @ sed -i \'s/ longjmp/ longjmp_crepair/g\' @\n',
-                'grep -nrl -e "rint" | grep "\.c" | xargs -I @ sed -i \'s/ rint/ rint_crepair/g\' @\n',
-                'grep -nrl -e "fabs_crepair" | grep "\.c" | xargs git add \n',
-                'grep -nrl -e "longjmp_crepair" | grep "\.c" | xargs git add \n',
-                'grep -nrl -e "rint_crepair" | grep "\.c" | xargs git add \n',
-                'git config --global user.email "you@example.com"\n',
-                'git config --global user.name "Your Name"\n',
-                "git commit -m 'replace with proxy functions'",
-            ],
-            inst_script_path,
-        )
+        content = []
+        for func_name in ["rint", "fabs", "longjmp"]:
+            content.append(
+                'grep -nrl -e "{0}" | grep "\.c" | '
+                "xargs -I @ sed -i "
+                "-e 's/ {0}(/ {0}_crepair(/g' "
+                "-e 's/\t{0}(/\t{0}_crepair(/g'"
+                " @\n".format(func_name)
+            )
+            content.append(
+                'grep -nrl -e "{0}_crepair" | grep "\.c" | xargs git add \n'.format(
+                    func_name
+                )
+            )
+
+        content.append('git config --global user.email "you@example.com"\n')
+        content.append('git config --global user.name "Your Name"\n')
+        content.append("git commit -m 'replace with proxy functions'")
+        self.write_file(content, inst_script_path)
         reconfig_command = "bash {}".format(inst_script_path)
         self.run_command(
             reconfig_command, log_file_path=self.log_instrument_path, dir_path=dir_src
@@ -55,9 +60,9 @@ class CrashRepair(AbstractLocalizeTool):
         )
         return
 
-    def rerun_configuration(self, config_script: str) -> None:
-        self.emit_normal("re-running configuration")
-        _config_path = self.dir_expr + f"/{self.name}-config"
+    def rerun_configuration(self, config_script: str, build_script: str) -> None:
+        self.emit_normal("re-running configuration and build")
+        _config_path = self.dir_expr + f"/{self.name}-config-build"
         dir_src = join(self.dir_expr, "src")
         self.write_file(
             [
@@ -65,48 +70,39 @@ class CrashRepair(AbstractLocalizeTool):
                 f"cd {dir_src}\n",
                 "make distclean; rm -f CMakeCache.txt\n",
                 f"CC=crepair-cc CXX=crepair-cxx {config_script} {self.dir_expr}\n",
+                f"CC=crepair-cc CXX=crepair-cxx {build_script} {self.dir_expr}\n",
             ],
             _config_path,
         )
         reconfig_command = "bash {}".format(_config_path)
-        log_reconfig_path = join(self.dir_logs, f"{self.name}-re-config.log")
+        log_reconfig_path = join(self.dir_logs, f"{self.name}-re-config-build.log")
         self.run_command(reconfig_command, log_file_path=log_reconfig_path)
 
     def generate_conf_file(self, bug_info: Dict[str, Any]) -> str:
-        config_script = bug_info.get(self.key_config_script, None)
-        if not config_script:
-            self.error_exit(f"{self.name} requires a configuration script as input")
-
         repair_conf_path = join(self.dir_expr, "crashrepair.conf")
         conf_content = []
         if self.key_exploit_list not in bug_info:
             self.error_exit("CrashRepair requires a proof of concept")
         poc_list = bug_info[self.key_exploit_list]
         poc_abs_list = [join(self.dir_setup, x) for x in poc_list]
-        build_command = bug_info.get(self.key_build_command, None)
-        if build_command is None:
-            build_script = bug_info.get(self.key_build_script, None)
-            if not build_script:
-                self.error_exit(
-                    f"{self.name} requires a build script or build command" f"as input"
-                )
-            build_command = f"{self.dir_setup}/{build_script}"
-
         conf_content.append("dir_exp:{}\n".format(self.dir_expr))
         conf_content.append("tag_id:{}\n".format(bug_info[self.key_bug_id]))
         conf_content.append("src_directory:src\n")
         conf_content.append("binary_path:{}\n".format(bug_info[self.key_bin_path]))
-        conf_content.append(
-            f"config_command:CC=crepair-cc CXX=crepair-cxx {self.dir_setup}/{config_script}\n"
-        )
-        conf_content.append(
-            "build_command:CC=crepair-cc CXX=crepair-cxx {}\n".format(build_command)
-        )
+        conf_content.append("config_command:skip\n")
+        conf_content.append("build_command:skip\n")
         if self.key_crash_cmd not in bug_info:
             self.error_exit("CrashRepair requires a test input list")
 
         conf_content.append("test_input_list:{}\n".format(bug_info[self.key_crash_cmd]))
         conf_content.append("poc_list:{}\n".format(",".join(poc_abs_list)))
+        conf_content.append(
+            "klee_flags:--link-llvm-lib=/CrashRepair/lib/libjpeg-8.4.bca "
+            "--link-llvm-lib=/CrashRepair/lib/libz.bca "
+            "--link-llvm-lib=/CrashRepair/lib/liblzma.bca "
+            "--link-llvm-lib=/CrashRepair/lib/libjbig.bca "
+            "--link-llvm-lib=/CrashRepair/lib/libcrepair_proxy.bca\n"
+        )
         self.append_file(conf_content, repair_conf_path)
         return repair_conf_path
 
@@ -117,10 +113,13 @@ class CrashRepair(AbstractLocalizeTool):
         config_script = bug_info.get(self.key_config_script, None)
         if not config_script:
             self.error_exit(f"{self.name} requires a configuration script as input")
+        build_script = bug_info.get(self.key_build_script, None)
+        if not build_script:
+            self.error_exit(f"{self.name} requires a build script as input")
 
-        self.instrument(bug_info)
         config_script = join(self.dir_setup, config_script)
-        self.rerun_configuration(config_script)
+        build_script = join(self.dir_setup, build_script)
+        self.rerun_configuration(config_script, build_script)
         timeout_h = str(task_config_info[self.key_timeout])
         timeout_m = 60 * float(timeout_h)
         timeout_validation = int(timeout_m * 0.75)
@@ -129,6 +128,7 @@ class CrashRepair(AbstractLocalizeTool):
         patch_limit = 10
         bug_json_path = self.dir_expr + "/bug.json"
         self.timestamp_log_start()
+        self.emit_normal(f"running {self.name}")
         repair_command = (
             f"bash -c 'stty cols 100 && stty rows 100 && timeout -k 5m {str(timeout_h)}h "
             f"crepair --conf={conf_path} "
