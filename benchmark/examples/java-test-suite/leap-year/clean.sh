5324fe5329adf76bc4c8fddb3717f4a77384b812
Martin Mirchev <mirchevmartin2203@gmail.com>
2024-05-13 19:31:03 +0800

    Update leap year to work remotely

--- benchmark/examples/java-test-suite/leap-year/clean.sh
+++ benchmark/examples/java-test-suite/leap-year/clean.sh
@@ -3,7 +3,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
-dir_name=/experiment/$benchmark_name/$project_name/$bug_id
+dir_name=$EXPERIMENT_DIR/$benchmark_name/$project_name/$bug_id
 cd $dir_name/src
 
 make clean
