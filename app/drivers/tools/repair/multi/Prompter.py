b100735778e1ecd09b6b3c4f9f2b293dbc73ff73
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2024-07-03 10:51:40 +0800

    write meta-data

--- app/drivers/tools/repair/multi/Prompter.py
+++ app/drivers/tools/repair/multi/Prompter.py
@@ -118,19 +118,19 @@ class Prompter(AbstractRepairTool):
 
     def create_meta_data(self) -> None:
         patch_info_json = join(self.dir_output, "generated_patches.json")
+        patch_list = []
         if self.is_file(patch_info_json):
             patch_info = self.read_json(patch_info_json)
-            patch_list = []
             if isinstance(patch_info, Dict):
                 patch_list = patch_info["patches"]
-            metadata = {
-                "patches_dir": join(self.dir_output, "patches"),
-                "patches": patch_list,
-            }
-            self.write_json(
-                metadata,
-                join(self.dir_output, "meta-data.json"),
-            )
+        metadata = {
+            "patches_dir": join(self.dir_output, "patches"),
+            "patches": patch_list,
+        }
+        self.write_json(
+            metadata,
+            join(self.dir_output, "meta-data.json"),
+        )
 
     def invoke(
         self, bug_info: Dict[str, Any], task_config_info: Dict[str, Any]
