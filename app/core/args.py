nus-apr/cerberus

commit b9fb4caf87414632e744f068781482ccd915f96d

Add composite workflows and updates in tools (#198)
* allow image hash to be updated using config file

* Update Infer driver

* add ZAP driver

* run config command if present

* add instrument script

* Format driver

* set prebuilt image for aprcomp benchmarks

* add slicing tool

* add slicing dir

* change path from command issued

* add crashrepair localize tool

* Add the slice type to the typing module

* move examples benchmark to multi

* update driver

* update benchmark example

* update localization stats

* update crashrepair localizer driver

* Update vulnloc reference

* Resolved hard-coding issue in vulnloc benchmark

* Update reference

* Update EffFix,SAVER,FootPatch driver for the benchmark change

* Fix dict access in abstract benchmark

* Fix typo in EffFix driver

* Update efffix benchmark

* Fix more dict access in abstract tool

* Fix several path issues in EffFix driver

* add aixcc prompt (#175)

* AIxCC framework using the fine-tuned DeepSeek model (#176)

* add aixcc prompt

* add sft deepseek model

* Remove requirements for having tests in benchmark

* Fix count_neg and count_pos

* Remove efffix bench temporarily

* Re-add efffix benchmark

* Composite Workflows, New Metadata format, Refactoring (#173)

* Dict is unhashable

* Another typing fix and path

* Fix some small things

* Remove return statement

* add instrumentation pass for AbstractLocalizeTool

* Update Gzoltar output

* Add identifier creation from GZoltar and fix a future bug

* make ArjaE use localization metadata

* fix ArjaE localizaition data; use shorter testing timeout

* fix ArjaE localizaition data; use shorter testing timeout

* add missing import in ArjaE driver

* make TBar accept localization data

* modify TBar driver

* Fix a format error

* Refactor Cerberus - remove duplicated code and add typings

* Arguments not passed correctly in execute_wrapped

app/core/args.py
@@ -1,7 +1,10 @@
 import argparse
 import multiprocessing
+from argparse import Action
 from argparse import HelpFormatter
+from argparse import Namespace
 from operator import attrgetter
+from typing import Iterable
 
 from app.core import definitions
 from app.core import utilities
@@ -9,12 +12,12 @@ from app.core import values
 
 
 class SortingHelpFormatter(HelpFormatter):
-    def add_arguments(self, actions):
+    def add_arguments(self, actions: Iterable[Action]) -> None:
         actions = sorted(actions, key=attrgetter("option_strings"))
         super(SortingHelpFormatter, self).add_arguments(actions)
 
 
-def parse_args():
+def parse_args() -> Namespace:
     parser = argparse.ArgumentParser(
         prog=values.tool_name,
         usage="%(prog)s [options]",
@@ -45,6 +48,14 @@ def parse_args():
     )
 
     optional.add_argument(
+        definitions.ARG_SUBJECT_BASED,
+        "-sb",
+        help="experiment image based on subject image\n\n",
+        action="store_true",
+        default=False,
+    )
+
+    optional.add_argument(
         definitions.ARG_CONFIG_FILE, "-c", type=str, help="Path to the JSON config file"
     )
 
@@ -54,6 +65,11 @@ def parse_args():
         help="Activate Textual UI",
         action="store_true",
         default=False,
+    )
+
+    optional.add_argument(
+        definitions.ARG_SUBSEQUENCE,
+        help="Use a subsequence of the composite workflow. Format is startInclusive-endInclusive",
     )
 
     optional.add_argument(
@@ -98,6 +114,13 @@ def parse_args():
     optional.add_argument(
         definitions.ARG_DOCKER_HOST,
         help="custom URL for the docker server which will host the containers",
+    )
+
+    optional.add_argument(
+        definitions.KEY_TIMESTAMP,
+        help="Add timestamp before every command",
+        acton="store_true",
+        default=False,
     )
 
     # TODO: Group list of tools based on type
@@ -186,6 +209,13 @@ def parse_args():
     optional.add_argument(
         definitions.ARG_RUN_TESTS_ONLY,
         help="only run the tests of the experiment",
+        action="store_true",
+        default=False,
+    )
+
+    optional.add_argument(
+        definitions.ARG_USE_SUBJECT_AS_BASE,
+        help="use the subject image as the base image",
         action="store_true",
         default=False,
     )
@@ -280,7 +310,6 @@ def parse_args():
     optional.add_argument(definitions.ARG_BUG_ID, help="identifier of the bug")
     optional.add_argument(
         definitions.ARG_BUG_ID_LIST,
-        type=list,  # type: ignore
         help="list of identifiers for the bugs",
         nargs="+",
         default=[],

app/core/configs/general/GeneralConfig.py
@@ -17,7 +17,7 @@ class GeneralConfig:
         self.cpus = cpus
         self.gpus = gpus
 
-    def __str__(self):
+    def __str__(self) -> str:
         return (
             f"[general config] parallel Mode: {self.parallel_mode}\n"
             f"[general config] UI Mode: {self.ui_mode}\n"

commit 178e31ee27f9df23d27cec19559f91f1b5cb7999

Added timestamp arg in GeneralConfig and fixed a typo
b9fb4caf87414632e744f068781482ccd915f96d introduced, both, a typo when
adding "timestamp" to the options. Also, even though passed via
ConfigDataFactory.create._create_general_config, timestamp arg wasn't
added to GeneralConfig.py

app/core/args.py
@@ -119,7 +119,7 @@ def parse_args() -> Namespace:
     optional.add_argument(
         definitions.KEY_TIMESTAMP,
         help="Add timestamp before every command",
-        acton="store_true",
+        action="store_true",
         default=False,
     )
 

app/core/configs/general/GeneralConfig.py
@@ -7,6 +7,7 @@ class GeneralConfig:
         secure_hash: bool,
         cpus: int,
         gpus: int,
+        timestamp: bool,
     ):
 
         # validate config
@@ -16,6 +17,7 @@ class GeneralConfig:
         self.secure_hash = secure_hash
         self.cpus = cpus
         self.gpus = gpus
+        self.timestamp = timestamp
 
     def __str__(self) -> str:
         return (